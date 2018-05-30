52573e5909f13d0018528ffe734fed16c7ee1d5f
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2018-05-30 16:13:05 +0200

    Created package metadata in utils
    + code cleanup

---
+++ utils/consts.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"time"
+)
+
+const (
+	address = "localhost:50051"
+	//TimeoutCtxDefault default timeout for grpc command invocation
+	TimeoutCtxDefault = 20 * time.Second
+	//TimeoutCtxVM timeout for grpc command relative to VM creation
+	TimeoutCtxVM = 2 * time.Minute
+	//MetadataContainerName is the name of the Object Storage Container/Bucket used to store metadata
+	MetadataContainerName string = "0.safescale"
+)
