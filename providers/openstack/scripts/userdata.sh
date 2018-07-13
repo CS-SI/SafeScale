c55cbd5469b6977484cb68f5616028292b510615
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2018-07-13 16:44:14 +0200

    Reworked iptables management
    + Code cleanup

--- providers/openstack/scripts/userdata.sh
+++ providers/openstack/scripts/userdata.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Copyright 2018, CS Systemes d'Information, http://www.c-s.fr
 #
@@ -20,10 +20,41 @@ exec 2<&-
 exec 1<>/var/tmp/user_data.log
 exec 2>&1
 
+detect_facts() {
+   local -g LINUX_KIND=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2 | sed 's/"//g')
+   local -g VERSION_ID=$(cat /etc/os-release | grep "^VERSION_ID=" | cut -d= -f2 | sed 's/"//g')
+}
+detect_facts
+
+# Some functions and global variables related to iptables (the idea being to reduce size of generated
+# user_data.sh script transmitted to the host)
+fw_i_accept() {
+    iptables -A INPUT -j ACCEPT $*
+}
+fw_f_accept() {
+    iptables -A FORWARD -j ACCEPT $*
+}
+PR_IP=
+PR_IF=
+PU_IP=
+PU_IF=
+i_PR_IF=
+i_PU_IF=
+o_PR_IF=
+o_PU_IF=
+
+save_iptables_rules() {
+   case $LINUX_KIND in
+       rhel|centos) iptables-save >/etc/sysconfig/iptables ;;
+       debian|ubuntu) iptables-save >/etc/iptables/rules.v4 ;;
+   esac
+}
+
+# Creates user gpac
 create_user() {
-    echo "Creating user {{.User}}..."
+    echo "Creating user {{ .User }}..."
     useradd {{ .User }} --home-dir /home/{{ .User }} --shell /bin/bash --comment "" --create-home
-    echo gpac:{{.Password}} | chpasswd
+    echo gpac:{{ .Password }} | chpasswd
     echo "{{ .User }} ALL=(ALL) NOPASSWD:ALL" >>/etc/sudoers
 
     # Sets ssh config
@@ -40,15 +71,17 @@ create_user() {
     echo done
 }
 
+# Configure network for Debian distribution
 configure_network_debian() {
     echo "Configuring network (debian-based)..."
-    rm -f /etc/network/interfaces.d/50-cloud-init.cfg
-    mkdir -p /etc/network/interfaces.d
+    local path=/etc/network/interfaces.d cfg=$path/50-cloud-init.cfg
+    rm -f $cfg
+    mkdir -p $path
     # Configure all network interfaces in dhcp
     for IF in $(ls /sys/class/net); do
         if [ $IF != "lo" ]; then
-            echo "auto ${IF}" >> /etc/network/interfaces.d/50-cloud-init.cfg
-            echo "iface ${IF} inet dhcp" >> /etc/network/interfaces.d/50-cloud-init.cfg
+            echo "auto ${IF}" >>$cfg
+            echo "iface ${IF} inet dhcp" >>$cfg
         fi
     done
 
@@ -56,12 +89,13 @@ configure_network_debian() {
     echo done
 }
 
+# Configure network using netplan
 configure_network_netplan() {
     echo "Configuring network (netplan-based)..."
 
     mv -f /etc/netplan /etc/netplan.orig
     mkdir -p /etc/netplan
-    cat <<EOF >/etc/netplan/50-cloud-init.yaml
+    cat <<-'EOF' >/etc/netplan/50-cloud-init.yaml
 network:
   version: 2
   renderer: networkd
@@ -70,9 +104,9 @@ network:
       dhcp4: true
     ens4:
       dhcp4: true
-{{if .GatewayIP}}
-      gateway4: {{.GatewayIP}}
-{{end}}
+{{- if .GatewayIP }}
+      gateway4: {{ .GatewayIP }}
+{{- end }}
 EOF
     netplan generate
     netplan apply
@@ -80,18 +114,15 @@ EOF
     echo done
 }
 
+# Configure network for redhat-like distributions (rhel, centos, ...)
 configure_network_redhat() {
-    echo "Configuring network (redhat-based)..."
+    echo "Configuring network (redhat-like)..."
 
     # We don't want NetworkManager
     systemctl disable NetworkManager &>/dev/null
     systemctl stop NetworkManager &>/dev/null
     yum remove -y NetworkManager &>/dev/null
     systemctl restart network
-
-    # We don't want firewalld
-    systemctl disable firewalld &>/dev/null
-    systemctl stop firewalld &>/dev/null
 
     # Configure all network interfaces in dhcp
     for IF in $(ls /sys/class/net); do
@@ -108,106 +139,133 @@ EOF
     echo done
 }
 
-configure_initial_firewall() {
-    # Change default policy for table filter chain INPUT to be DROP (block everything)
-    iptables -t filter --policy INPUT DROP
-    # Opens up the required (loopback comm, ping, ssh, established connection)
-    iptables -t filter -A INPUT -i lo -j ACCEPT
-    iptables -t filter -A INPUT -p icmp --icmp-type 8 -s 0/0 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-    iptables -t filter -A INPUT -p icmp --icmp-type 0 -s 0/0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-    iptables -t filter -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-    iptables -t filter -A INPUT -p tcp --dport ssh -j ACCEPT
+reset_iptables() {
+    case $LINUX_KIND in
+        debian|ubuntu)
+            systemctl stop ufw &>/dev/null
+            systemctl disable ufw &>/dev/null
+            apt purge ufw &>/dev/null
+            apt install -y iptables-persistent
+            ;;
+
+        rhel|centos)
+            systemctl disable firewalld &>/dev/null
+            systemctl stop firewalld &>/dev/null
+            systemctl mask firewalld &>/dev/null
+            yum remove -y firewalld &>/dev/null
+            yum install -y iptables-services
+            systemctl enable iptables
+            systemctl enable ip6tables
+            systemctl start iptables
+            systemctl start ip6tables
+            ;;
+    esac
+
+    # We flush the current firewall rules possibly introduced by iptables service
+    iptables-save | awk '/^[*]/ { print $1 }
+                         /^:[A-Z]+ [^-]/ { print $1 " ACCEPT" ; }
+                         /COMMIT/ { print $0; }' | iptables-restore
+}
+
+install_squid() {
+    case $LINUX_KIND in
+        rhel|centos)
+            yum install -y squid;;
+        debian|ubuntu)
+            apt-get install -y squid;;
+    esac
+
+    for IF in lo $PR_IF; do
+        iptables -t nat -A PREROUTING -i $IF -p tcp -m multiport --dport 80,443 -j REDIRECT --to-port 3128
+        fw_i_accept -m state --state NEW,ESTABLISHED,RELATED -i $IF -p tcp --dport 3128
+    done
+    #iptables -A OUTPUT -j ACCEPT -m state --state NEW,ESTABLISHED,RELATED -o $PU_IF -p tcp --dport 80
+    fw_i_accept -m state --state ESTABLISHED,RELATED $i_PU_IF -p tcp -m multiport --sport 80,443
+    #iptables -A OUTPUT -j ACCEPT -m state --state ESTABLISHED,RELATED -o $PR_IF -p tcp --sport 80
 }
 
 configure_as_gateway() {
     echo "Configuring host as gateway..."
-    PUBLIC_IP=$(curl ipinfo.io/ip 2>/dev/null)
-    PUBLIC_IF=$(netstat -ie | grep -B1 ${PUBLIC_IP} | head -n1 | awk '{print $1}')
-    PUBLIC_IF=${PUBLIC_IF%%:}
 
-    PRIVATE_IP=
+    reset_iptables
+
+    # Change default policy for table filter chain INPUT to be DROP (block everything)
+    iptables -P INPUT DROP
+    # Opens up the required (loopback comm, ping, ssh, established connection)
+    fw_i_accept -i lo
+    fw_i_accept -p icmp --icmp-type 8 -s 0/0 -m state --state NEW,ESTABLISHED,RELATED
+    fw_i_accept -p icmp --icmp-type 0 -s 0/0 -m state --state ESTABLISHED,RELATED
+    fw_i_accept -m conntrack --ctstate ESTABLISHED,RELATED
+    fw_i_accept -p tcp --dport ssh
+
+    PU_IP=$(curl ipinfo.io/ip 2>/dev/null)
+    PU_IF=$(netstat -ie | grep -B1 ${PU_IP} | head -n1 | awk '{print $1}')
+    PU_IF=${PU_IF%%:}
+
     for IF in $(ls /sys/class/net); do
-        if [ ${IF} != "lo" ] && [ ${IF} != "${PUBLIC_IF}" ]; then
-            PRIVATE_IP=$(ip a |grep ${IF} | grep inet | awk '{print $2}' | cut -d '/' -f1)
+        if [ ${IF} != "lo" ] && [ ${IF} != "${PU_IF}" ]; then
+            PR_IP=$(ip a |grep ${IF} | grep inet | awk '{print $2}' | cut -d '/' -f1)
         fi
     done
 
-    [ -z ${PRIVATE_IP} ] && return 1
+    [ -z ${PR_IP} ] && return 1
 
-    PRIVATE_IF=$(netstat -ie | grep -B1 ${PRIVATE_IP} | head -n1 | awk '{print $1}')
-    PRIVATE_IF=${PRIVATE_IF%%:}
+    PR_IF=$(netstat -ie | grep -B1 ${PR_IP} | head -n1 | awk '{print $1}')
+    PR_IF=${PR_IF%%:}
 
-    if [ ! -z $PRIVATE_IF ]; then
-        grep -v "net.ipv4.ip_forward=" /etc/sysctl.conf >/etc/sysctl.conf.new
-        mv /etc/sysctl.conf.new /etc/sysctl.conf
+    if [ ! -z $PR_IF ]; then
+        # Enable forwarding
         for i in /etc/sysctl.d/* /etc/sysctl.conf; do
             grep -v "net.ipv4.ip_forward=" $i >${i}.new
             mv -f ${i}.new ${i}
         done
         echo "net.ipv4.ip_forward=1" >/etc/sysctl.d/98-forward.conf
+        systemctl restart systemd-sysctl
 
-        cat <<- EOF >/sbin/routing
-#!/bin/sh -
-echo "activate routing"
-iptables -t nat -A POSTROUTING -o ${PUBLIC_IF} -j MASQUERADE
-iptables -A FORWARD -i ${PRIVATE_IF} -o ${PUBLIC_IF} -j ACCEPT
-iptables -A FORWARD -i ${PUBLIC_IF} -o ${PRIVATE_IF} -m state --state RELATED,ESTABLISHED -j ACCEPT
-EOF
-
-        cat <<- EOF >/etc/systemd/system/routing.service
-[Unit]
-Description=activate routing from ${PRIVATE_IF} to ${PUBLIC_IF}
-After=network.target
-
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/sbin/routing
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-        if [ -z $PUBLIC_IF ]; then
-            sed -i 's/-o  -j/-j/g; s/-i  -o/-o/g' /sbin/routing
-            sed -i 's/(Description=.*) to $/\\1/g' /etc/systemd/system/routing.service
-        fi
-
-        chmod u+x /sbin/routing
-
-        systemctl enable routing
-        systemctl start routing
+        # Routing
+        o_PR_IF="-o $PR_IF"
+        i_PR_IF="-i $PR_IF"
+        [ ! -z $PU_IF ] && o_PU_IF="-o $PU_IF" && i_PU_IF="-i $PU_IF"
+        iptables -t nat -A POSTROUTING -j MASQUERADE $o_PU_IF
+        fw_f_accept $i_PR_IF $o_PU_IF
+        fw_f_accept $i_PU_IF $o_PR_IF -m state --state RELATED,ESTABLISHED
     fi
+
+    #install_squid
+
+    save_iptables_rules
 
     echo done
 }
 
 configure_dns_legacy() {
-    cat <<-EOF > /etc/resolv.conf
-{{.ResolvConf}}
+    cat <<-'EOF' > /etc/resolv.conf
+{{ .ResolvConf }}
 EOF
 }
 
 configure_dns_resolvconf() {
-    cat <<-EOF >/etc/resolvconf/resolv.conf.d/original
-{{.ResolvConf}}
+    cat <<-'EOF' >/etc/resolvconf/resolv.conf.d/original
+{{ .ResolvConf }}
 EOF
     rm -f /etc/resolvconf/resolv.conf.d/tail
     cd /etc/resolvconf/resolv.conf.d && /etc/resolvconf/update.d/libc
 }
 
 configure_gateway() {
-    echo "Configuring default router to {{.GatewayIP}}"
+    echo "Configuring default router to {{ .GatewayIP }}"
+
+    reset_iptables
 
     route del -net default
 
-    cat <<- EOF > /sbin/gateway
+    cat <<-'EOF' > /sbin/gateway
 #!/bin/sh -
 echo "configure default gateway"
-/sbin/route add -net default gw {{.GatewayIP}}
+/sbin/route add -net default gw {{ .GatewayIP }}
 EOF
     chmod u+x /sbin/gateway
-    cat <<- EOF > /etc/systemd/system/gateway.service
+    cat <<-'EOF' > /etc/systemd/system/gateway.service
 Description=create default gateway
 After=network.target
 
@@ -224,67 +282,54 @@ EOF
     echo done
 }
 
-LINUX_KIND=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2 | sed 's/"//g')
-VERSION_ID=$(cat /etc/os-release | grep "^VERSION_ID=" | cut -d= -f2 | sed 's/"//g')
-
-echo "ConfIF={{.ConfIF}}"
-echo "IsGateway={{.IsGateway}}"
-echo "AddGateway={{.AddGateway}}"
-
 case $LINUX_KIND in
     debian)
         create_user
-        {{if .ConfIF}}
+        {{- if .ConfIF }}
         configure_network_debian
-        {{end}}
-        {{if .IsGateway}}
+        {{- end }}
+        {{- if .IsGateway }}
         configure_as_gateway
-        systemctl restart systemd-sysctl
-        {{end}}
-        {{if .AddGateway}}
-        configure_gateway
+        {{- else if .AddGateway }}
         configure_dns_legacy
-        {{end}}
+        configure_gateway
+        {{- end }}
         ;;
 
     ubuntu)
         create_user
-        {{if .ConfIF}}
+        {{- if .ConfIF }}
         if [[ "$VERSION_ID" = "17.10" || "$VERSION_ID" = "18.04" ]]; then
             configure_network_netplan
         else
             configure_network_debian
         fi
-        {{end}}
-        {{if .IsGateway}}
+        {{- end }}
+        {{- if .IsGateway }}
         configure_as_gateway
-        systemctl restart systemd-sysctl
-        {{end}}
-        {{if .AddGateway}}
-        configure_gateway
+        {{- else if .AddGateway }}
         configure_dns_resolvconf
-        {{end}}
+        configure_gateway
+        {{- end }}
         ;;
 
     redhat|centos)
         create_user
-        {{if .ConfIF}}
+        {{- if .ConfIF }}
         configure_network_redhat
-        {{end}}
-        {{if .IsGateway}}
+        {{- end }}
+        {{- if .IsGateway }}
         configure_as_gateway
-        systemctl restart systemd-sysctl
-        {{end}}
-        {{if .AddGateway}}
-        configure_gateway
+        {{- else if .AddGateway }}
         configure_dns_legacy
-        {{end}}
+        configure_gateway
+        {{- end }}
         ;;
     *)
         echo "Unsupported Linux distribution '$LINUX_KIND'!"
         exit 1
         ;;
 esac
-configure_initial_firewall
 
+>/var/tmp/userdata.done
 exit 0
