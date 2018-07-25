6d6183a8dfdf703a50c504a50284a055834ce720
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2018-07-25 10:42:19 +0200

    Added flavor BOH
    - Removed go-generate with stringer to implement String()
    - Introduced Parse() to return Flavor enum corresponding to string

--- perform/cluster/api/Flavor/enum.go
+++ perform/cluster/api/Flavor/enum.go
@@ -16,17 +16,57 @@
 
 package Flavor
 
-//go:generate stringer -type=Enum
+import (
+	"fmt"
+	"strings"
+)
 
 //Enum represents the flavor of a cluster, in other words what technology is used behind the scene
 type Enum int
 
 const (
 	_ Enum = iota
-	//DCOS flavor
+	// DCOS managed cluster
 	DCOS
-	//Kubernetes flavor for a pure Kubernetes cluster
+	// Kubernetes flavor for a pure Kubernetes cluster
 	Kubernetes
-	//Swarm flavor for a pure docker swarm cluster
+	// Swarm flavor for a pure docker swarm cluster
 	Swarm
+	// BOH for a "Bunch Of Hosts", without cluster management
+	BOH
 )
+
+var (
+	parseMap = map[string]Enum{
+		"dcos":       DCOS,
+		"kubernetes": Kubernetes,
+		"swarm":      Swarm,
+		"boh":        BOH,
+	}
+
+	stringMap = map[Enum]string{
+		DCOS:       "DCOS",
+		Kubernetes: "Kubernetes",
+		Swarm:      "Swarm",
+		BOH:        "BOH",
+	}
+)
+
+// Parse returns a Flavor.Enum corresponding to the content of the parameter
+func Parse(v string) (e Enum, err error) {
+	var found bool
+	lowered := strings.ToLower(v)
+	if e, found = parseMap[lowered]; found {
+		err = nil
+	} else {
+		err = fmt.Errorf("Unknown Flavor '%s'", v)
+	}
+	return
+}
+
+func (e Enum) String() string {
+	if str, found := stringMap[e]; found {
+		return str
+	}
+	return ""
+}
