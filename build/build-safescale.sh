f994fbf6862e07e86e83f92263bb61bd4ccce46a
saimecs <stephan.aime@c-s.fr>
2018-08-02 11:24:05 +0200

    Add log messages

--- build/build-safescale.sh
+++ build/build-safescale.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
 # ----------------------
-# Creation working directory
+# Create working directory
 # ----------------------
+echo "Create working directory"
 WRKDIR=${GOPATH=/go}/src/github.com/CS-SI
 mkdir -p ${WRKDIR}
 cd ${WRKDIR}
@@ -11,10 +12,11 @@ rm -rf SafeScale
 # ----------------------
 # Get source code
 # ----------------------
+echo "Get source code"
 BRANCH_NAME=${BRANCH_NAME:="develop"}
-echo "Cloning branch ${BRANCH_NAME}"
-
 GIT_REPO_URL=${GIT_REPO_URL:="https://github.com/CS-SI/SafeScale.git"}
+echo "Cloning branch '${BRANCH_NAME}' from repo '${GIT_REPO_URL}'"
+
 git clone ${GIT_REPO_URL} -b ${BRANCH_NAME} --depth=1
 
 cd SafeScale
@@ -22,16 +24,19 @@ cd SafeScale
 # ----------------------
 # Get dependencies
 # ----------------------
+echo "Get dependencies"
 make deps
 
 # ----------------------
 # Compile
 # ----------------------
+echo "Compile"
 make
 
 # ----------------------
 # Install
 # ----------------------
+echo "Install"
 export GOBIN=${GOBIN=/go/bin}
 make install
 
@@ -39,6 +44,7 @@ make install
 # Copy produced binaries to export directory
 # ----------------------
 EXPDIR=/usr/local/safescale/bin
+echo "Copy produced binaries to export directory '${EXPDIR}'"
 mkdir -p ${EXPDIR}
 cp ${GOBIN}/broker ${EXPDIR}
 cp ${GOBIN}/brokerd ${EXPDIR}
