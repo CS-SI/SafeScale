573587e910a9fd9fe5ee36285010b05b45b0c23b
pierre crumeyrolle <pierre.crumeyrolle@c-s.fr>
2018-08-10 16:31:27 +0200

    modif build docker file:wq

--- build/create-docker.sh
+++ build/create-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 WRKDIR=$(readlink -f $(dirname "$0"))
-docker build --rm -f ${WRKDIR}/Dockerfile -t safescale:latest ${WRKDIR}
+docker build --rm --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f ${WRKDIR}/Dockerfile -t safescale:latest ${WRKDIR}
