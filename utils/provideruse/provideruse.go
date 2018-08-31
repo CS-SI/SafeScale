56cff996880a0375eedcdea855589333d722b2fe
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2018-08-31 11:28:50 +0200

    Added WaitSSHServerReady() : wait the availability of ssh on remote host

--- utils/provideruse/provideruse.go
+++ utils/provideruse/provideruse.go
@@ -17,12 +17,14 @@
 package provideruse
 
 import (
+	"fmt"
+
 	brokerclient "github.com/CS-SI/SafeScale/broker/client"
 
 	"github.com/CS-SI/SafeScale/providers"
 )
 
-//GetProviderService returns the service provider corresponding to the current Tenant
+// GetProviderService returns the service provider corresponding to the current Tenant
 func GetProviderService() (*providers.Service, error) {
 	tenant, err := brokerclient.New().Tenant.Get(brokerclient.DefaultTimeout)
 	if err != nil {
@@ -33,4 +35,21 @@ func GetProviderService() (*providers.Service, error) {
 		return nil, err
 	}
 	return svc, nil
+}
+
+// WaitSSHServerReady waits for SSH server to be ready. After timeout, fails
+// The 'timeout' parameter is in minutes
+func WaitSSHServerReady(svc *providers.Service, hostID string, timeout uint8) error {
+	var err error
+	if svc == nil {
+		svc, err = GetProviderService()
+		if err != nil {
+			return err
+		}
+	}
+	ssh, err := svc.GetSSHConfig(hostID)
+	if err != nil {
+		return fmt.Errorf("failed to read SSH config: %s", err.Error())
+	}
+	return ssh.WaitServerReady(timeout)
 }
