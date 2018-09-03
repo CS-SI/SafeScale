d55d49aac81670fa63b21403a2335c4d0beab24e
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2018-09-03 09:55:43 +0200

    Added some functions in ~gpac/.bashrc
    - pathremove removes a path from PATH
    - pathprepend prepends a path to PATH
    - pathappend appends a path to PATH
    - all these functions avoid duplicates in PATH

--- providers/userdata/scripts/userdata.sh
+++ providers/userdata/scripts/userdata.sh
@@ -67,6 +67,31 @@ create_user() {
     # Create flag file to deactivate Welcome message on ssh
     touch /home/{{ .User }}/.hushlogin
 
+    cat >>/home/gpac/.bashrc <<-'EOF'
+pathremove() {
+        local IFS=':'
+        local NEWPATH
+        local DIR
+        local PATHVARIABLE=${2:-PATH}
+        for DIR in ${!PATHVARIABLE} ; do
+                if [ "$DIR" != "$1" ] ; then
+                  NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
+                fi
+        done
+        export $PATHVARIABLE="$NEWPATH"
+}
+pathprepend() {
+        pathremove $1 $2
+        local PATHVARIABLE=${2:-PATH}
+        export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
+}
+pathappend() {
+        pathremove $1 $2
+        local PATHVARIABLE=${2:-PATH}
+        export $PATHVARIABLE="${!PATHVARIABLE:+${!PATHVARIABLE}:}$1"
+}
+pathprepend $HOME/.local/bin
+EOF
     # Ensures ownership
     chown -R {{ .User }}:{{ .User }} /home/{{ .User }}
     echo done
