496b07f8ff4b586524501e075387d8dbb09c125a
sdorgan <sebastien.dorgan@c-s.fr>
2018-09-07 14:00:02 +0200

    improve ssh copy and ssh run

--- broker/client/ssh.go
+++ broker/client/ssh.go
@@ -17,6 +17,8 @@
 package client
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	pb "github.com/CS-SI/SafeScale/broker"
@@ -31,41 +33,148 @@ type ssh struct {
 	session *Session
 }
 
+// func systemSSH(bsc *broker.SshConfig, err error) (*system.SSHConfig, error) {
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	if bsc == nil {
+// 		return nil, nil
+// 	}
+
+// 	g, _ := systemSSH(bsc.Gateway, nil)
+// 	return &system.SSHConfig{
+// 		GatewayConfig: g,
+// 		Host:          bsc.Host,
+// 		Port:          int(bsc.Port),
+// 		PrivateKey:    bsc.PrivateKey,
+// 		User:          bsc.User,
+// 	}, nil
+// }
+
 // Run ...
 func (s *ssh) Run(hostName, command string, timeout time.Duration) (int, string, string, error) {
-	conn := utils.GetConnection()
-	defer conn.Close()
 	if timeout < utils.TimeoutCtxHost {
 		timeout = utils.TimeoutCtxHost
 	}
-	ctx, cancel := utils.GetContext(timeout)
-	defer cancel()
-	service := pb.NewSshServiceClient(conn)
-	resp, err := service.Run(ctx, &pb.SshCommand{
-		Host:    &pb.Reference{Name: hostName},
-		Command: command,
-	})
+
+	host := &host{session: s.session}
+	cfg, err := host.SSHConfig(hostName)
 	if err != nil {
-		return -1, "", "", err
+		return 0, "", "", err
 	}
-	return int(resp.GetStatus()), resp.GetOutputStd(), resp.GetOutputErr(), nil
+	ssh := conv.ToAPISshConfig(cfg)
+
+	var stdOut, stdErr string
+	var retCode int
+
+	err = retry.WhileUnsuccessfulDelay1SecondWithNotify(
+		func() error {
+			// Create the command
+			sshCmd, err := ssh.Command(command)
+			if err != nil {
+				return err
+			}
+			retCode, stdOut, stdErr, err = sshCmd.Run()
+			return err
+		},
+		2*time.Minute,
+		retry.NotifyByLog)
+
+	return retCode, stdOut, stdErr, err
+}
+
+const protocolSeparator = ":"
+
+func extracthostName(in string) (string, error) {
+	parts := strings.Split(in, protocolSeparator)
+	if len(parts) == 1 {
+		return "", nil
+	}
+	if len(parts) > 2 {
+		return "", fmt.Errorf("too many parts in path")
+	}
+	hostName := strings.TrimSpace(parts[0])
+	for _, protocol := range []string{"file", "http", "https", "ftp"} {
+		if strings.ToLower(hostName) == protocol {
+			return "", fmt.Errorf("no protocol expected. Only host name")
+		}
+	}
+
+	return hostName, nil
+}
+
+func extractPath(in string) (string, error) {
+	parts := strings.Split(in, protocolSeparator)
+	if len(parts) == 1 {
+		return in, nil
+	}
+	if len(parts) > 2 {
+		return "", fmt.Errorf("too many parts in path")
+	}
+	_, err := extracthostName(in)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(parts[1]), nil
 }
 
 // Copy ...
 func (s *ssh) Copy(from, to string, timeout time.Duration) error {
-	conn := utils.GetConnection()
-	defer conn.Close()
-	if timeout < utils.TimeoutCtxHost {
-		timeout = utils.TimeoutCtxHost
+	host := &host{session: s.session}
+
+	hostName := ""
+	var upload bool
+	var localPath, remotePath string
+	// Try extract host
+	hostFrom, err := extracthostName(from)
+	if err != nil {
+		return err
 	}
-	ctx, cancel := utils.GetContext(timeout)
-	defer cancel()
-	service := pb.NewSshServiceClient(conn)
-	command := pb.SshCopyCommand{
-		Source:      from,
-		Destination: to,
+	hostTo, err := extracthostName(to)
+	if err != nil {
+		return err
 	}
-	_, err := service.Copy(ctx, &command)
+
+	// Host checks
+	if hostFrom != "" && hostTo != "" {
+		return fmt.Errorf("copy between 2 hosts is not supported yet")
+	}
+	if hostFrom == "" && hostTo == "" {
+		return fmt.Errorf("no host name specified neither in from nor to")
+	}
+
+	fromPath, err := extractPath(from)
+	if err != nil {
+		return err
+	}
+	toPath, err := extractPath(to)
+	if err != nil {
+		return err
+	}
+
+	if hostFrom != "" {
+		hostName = hostFrom
+		remotePath = fromPath
+		localPath = toPath
+		upload = false
+	} else {
+		hostName = hostTo
+		remotePath = toPath
+		localPath = fromPath
+		upload = true
+	}
+
+	cfg, err := host.SSHConfig(hostName)
+	if err != nil {
+		return err
+	}
+	ssh := conv.ToAPISshConfig(cfg)
+
+	if err != nil {
+		return err
+	}
+	_, _, _, err = ssh.Copy(remotePath, localPath, upload)
 	return err
 }
 
