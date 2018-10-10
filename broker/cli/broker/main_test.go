a223d26020bebc6b2b54c9c5f7ea0492a3089ca6
Oscar <oscar.picas@gmail.com>
2018-10-10 11:32:35 +0200

    Added test binaries

---
+++ broker/cli/broker/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/dlespiau/covertool/pkg/cover"
+	"github.com/dlespiau/covertool/pkg/exit"
+)
+
+func TestMain(m *testing.M) {
+	cover.ParseAndStripTestFlags()
+
+	// Make sure we have the opportunity to flush the coverage report to disk when
+	// terminating the process.
+	exit.AtExit(cover.FlushProfiles)
+
+	// If the test binary name is "calc" we've are being asked to run the
+	// coverage-instrumented calc.
+	if path.Base(os.Args[0]) == "brokercc" {
+		main()
+		exit.Exit(0)
+	}
+
+	os.Exit(m.Run())
+}
