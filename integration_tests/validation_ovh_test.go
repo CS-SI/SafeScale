abe200b504840b4b766956140d2e7fca9ec92c01
Oscar <oscar.picas@gmail.com>
2018-10-12 16:01:11 +0200

    Issue: Improve error message when deleting a volume which is mounted #4

--- integration_tests/validation_ovh_test.go
+++ integration_tests/validation_ovh_test.go
@@ -234,3 +234,134 @@ func Test_Stop_Start(t *testing.T) {
 	require.Nil(t, err)
 	require.True(t, strings.Contains(out, "0 users"))
 }
+
+
+func Test_Delete_Volume_Mounted(t *testing.T) {
+	tearDown()
+	defer tearDown()
+
+	brokerd_launched, err := isBrokerdLaunched()
+	if !brokerd_launched {
+		fmt.Println("This requires that you launch brokerd in background and set the tenant")
+		require.True(t, brokerd_launched)
+	}
+	require.Nil(t, err)
+
+	in_path, err := canBeRun("broker")
+	require.Nil(t, err)
+
+	require.True(t, brokerd_launched)
+	require.True(t, in_path)
+
+	out, err := getOutput("broker tenant list")
+	require.Nil(t, err)
+	require.True(t, len(out) > 0)
+
+	out, err = getOutput("broker tenant get")
+	if err != nil {
+		fmt.Println("This requires that you set the right tenant before launching the tests")
+		require.Nil(t, err)
+	}
+	require.True(t, len(out) > 0)
+
+	out, err = getOutput("broker network list")
+	require.Nil(t, err)
+
+	fmt.Println("Creating network easy...")
+
+	out, err = getOutput("broker network create easy")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker network create easy")
+	require.NotNil(t, err)
+	require.True(t, strings.Contains(out, "A network already exist"))
+
+	fmt.Println("Creating VM easyVM...")
+
+	out, err = getOutput("broker host create easyvm --public --net easy")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker host create easyvm --public --net easy")
+	require.NotNil(t, err)
+	require.True(t, strings.Contains(out, "A host already exist"))
+
+	out, err = getOutput("broker host inspect easyvm")
+	require.Nil(t, err)
+
+	easyvm := HostInfo{}
+	json.Unmarshal([]byte(out), &easyvm)
+
+	fmt.Println("Creating VM complexvm...")
+
+	out, err = getOutput("broker host create complexvm --public --net easy")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker host create complexvm --public --net easy")
+	require.NotNil(t, err)
+	require.True(t, strings.Contains(out, "A host already exist"))
+
+	out, err = getOutput("broker nas list")
+	require.Nil(t, err)
+
+	fmt.Println("Creating NAS anastest...")
+
+	out, err = getOutput("broker nas  create anastest easyvm")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker nas  mount anastest complexvm")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker nas list")
+	require.Nil(t, err)
+	require.True(t, strings.Contains(out, "anastest"))
+
+	out, err = getOutput("broker nas inspect anastest")
+	require.Nil(t, err)
+
+	require.True(t, strings.Contains(out, "anastest"))
+	require.True(t, strings.Contains(out, "easyvm"))
+	require.True(t, strings.Contains(out, "complexvm"))
+
+	out, err = getOutput("broker nas  umount anastest complexvm")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker nas inspect anastest")
+	require.Nil(t, err)
+
+	require.True(t, strings.Contains(out, "anastest"))
+	require.True(t, strings.Contains(out, "easyvm"))
+	require.False(t, strings.Contains(out, "complexvm"))
+
+	out, err = getOutput("broker nas delete anastest ")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker nas list")
+	require.Nil(t, err)
+	require.False(t, strings.Contains(out, "anastest"))
+
+	out, err = getOutput("broker volume list")
+	require.Nil(t, err)
+	require.True(t, strings.Contains(out, "null"))
+
+	fmt.Println("Creating Volume volumetest...")
+
+	out, err = getOutput("broker volume  create volumetest")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker volume list")
+	require.Nil(t, err)
+	require.True(t, strings.Contains(out, "volumetest"))
+
+	out, err = getOutput("broker volume  attach  volumetest easyvm ")
+	require.Nil(t, err)
+
+	out, err = getOutput("broker volume delete volumetest")
+	require.NotNil(t, err)
+	require.True(t, strings.Contains(err.Error(), "still attached"))
+
+	// TODO Parse message received
+	message_received := "Could not delete volume 'volumetest': rpc error: code = Unknown desc = Error deleting volume: Bad request with: [DELETE https://volume.compute.sbg3.cloud.ovh.net/v1/7bf42a51e07a4be98e62b0435bfc1765/volumes/906e8b9c-b6ac-461b-9916-a8bc7afa8449], error message: {'badRequest': {'message': 'Volume 906e8b9c-b6ac-461b-9916-a8bc7afa8449 is still attached, detach volume first.', 'code': 400}}"
+	_ = message_received
+
+	fmt.Println(err.Error())
+}
