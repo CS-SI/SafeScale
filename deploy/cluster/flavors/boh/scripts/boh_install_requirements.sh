3d9b6ea34e6f22d91f076b7a7f6b0ef592f11315
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2018-10-21 22:00:06 +0200

    Added management of disabled features
    + adaptive timeout on some operations based on number of hosts
    + code cleanup

--- deploy/cluster/flavors/boh/scripts/boh_install_requirements.sh
+++ deploy/cluster/flavors/boh/scripts/boh_install_requirements.sh
@@ -70,7 +70,17 @@ case $LINUX_KIND in
         ;;
     debian|ubuntu)
         sfWaitForApt && apt update && \
-        sfWaitForApt && apt install -y curl wget time jq rclone unzip
+        sfWaitForApt && apt install -y curl wget time jq unzip
+        curl -kqSsL -O https://downloads.rclone.org/rclone-current-linux-amd64.zip && \
+        unzip rclone-current-linux-amd64.zip && \
+        cd rclone-*-linux-amd64 && \
+        cp rclone /usr/bin/ && \
+        rm -rf rclone-* && \
+        chown root:root /usr/bin/rclone && \
+        chmod 755 /usr/bin/rclone && \
+        mkdir -p /usr/local/share/man/man1 && \
+        cp rclone.1 /usr/local/share/man/man1/ && \
+        mandb
         ;;
     *)
         echo "unmanaged Linux distribution '$LINUX_KIND'"
