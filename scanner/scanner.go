c727a8d7d9f166b0f71366ada1cff8984dcb64c9
Oscar <oscar.picas@gmail.com>
2018-10-25 14:57:50 +0200

    Scanner now stores information in $HOME/.safescale/scanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -20,12 +20,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/CS-SI/SafeScale/providers/enums/IPVersion"
+	"github.com/CS-SI/SafeScale/utils"
+	"github.com/pkg/errors"
 	"io/ioutil"
 	"math"
 	"os"
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 	log "github.com/sirupsen/logrus"
 
@@ -63,8 +66,12 @@ var cmd = fmt.Sprintf("export LANG=C;echo $(%s)î$(%s)î$(%s)î$(%s)î$(%s)î$(%
 
 //CPUInfo stores CPU properties
 type CPUInfo struct {
+	TenantName   string `json:"tenant_name,omitempty"`
 	TemplateID   string `json:"template_id,omitempty"`
 	TemplateName string `json:"template_name,omitempty"`
+	ImageID   string `json:"image_id,omitempty"`
+	ImageName string `json:"image_name,omitempty"`
+	LastUpdated string `json:"last_updated,omitempty"`
 
 	NumberOfCPU    int     `json:"number_of_cpu,omitempty"`
 	NumberOfCore   int     `json:"number_of_core,omitempty"`
@@ -83,6 +90,57 @@ func parseOutput(output []byte) (*CPUInfo, error) {
 	str := strings.TrimSpace(string(output))
 
 	//	tokens := strings.Split(str, "\n")
+	tokens := strings.Split(str, "î")
+	if len(tokens) < 9 {
+		return nil, fmt.Errorf("parsing error: '%s'", str)
+	}
+	info := CPUInfo{}
+	var err error
+	info.NumberOfCPU, err = strconv.Atoi(tokens[0])
+	if err != nil {
+		return nil, fmt.Errorf("Parsing error: NumberOfCPU='%s' (from '%s')", tokens[0], str)
+	}
+	info.NumberOfCore, err = strconv.Atoi(tokens[1])
+	if err != nil {
+		return nil, fmt.Errorf("Parsing error: NumberOfCore='%s' (from '%s')", tokens[1], str)
+	}
+	info.NumberOfSocket, err = strconv.Atoi(tokens[2])
+	if err != nil {
+		return nil, fmt.Errorf("Parsing error: NumberOfSocket='%s' (from '%s')", tokens[2], str)
+	}
+	info.NumberOfCore = info.NumberOfCore * info.NumberOfSocket
+	info.CPUFrequency, err = strconv.ParseFloat(tokens[3], 64)
+	if err != nil {
+		return nil, fmt.Errorf("Parsing error: CpuFrequency='%s' (from '%s')", tokens[3], str)
+	}
+	info.CPUFrequency = math.Ceil(info.CPUFrequency/100) / 10
+
+	info.CPUArch = tokens[4]
+	info.Hypervisor = tokens[5]
+	info.CPUModel = tokens[6]
+	info.RAMSize, err = strconv.ParseFloat(tokens[7], 64)
+	if err != nil {
+		return nil, fmt.Errorf("Parsing error: RAMSize='%s' (from '%s')", tokens[7], str)
+	}
+	info.RAMSize = math.Ceil(info.RAMSize / 1024 / 1024)
+	info.RAMFreq, err = strconv.ParseFloat(tokens[8], 64)
+	if err != nil {
+		info.RAMFreq = 0
+	}
+	fmt.Println(tokens[9])
+	gpuTokens := strings.Split(tokens[9], "%")
+	nb := len(gpuTokens)
+	if nb > 1 {
+		info.GPUModel = strings.TrimSpace(gpuTokens[0])
+		info.GPU = nb - 1
+	}
+
+	return &info, nil
+}
+
+func createCPUInfo(output string) (*CPUInfo, error) {
+	str := strings.TrimSpace(output)
+
 	tokens := strings.Split(str, "î")
 	if len(tokens) < 9 {
 		return nil, fmt.Errorf("parsing error: '%s'", str)
@@ -347,6 +405,173 @@ func Run() {
 	fmt.Println("\nScanning done.")
 }
 
+func RunScanner() {
+	var targeted_providers []string
+	the_providers, _ := providers.Tenants()
+
+	for tenantName := range the_providers {
+		if strings.Contains(tenantName, "test") {
+			targeted_providers = append(targeted_providers, tenantName)
+		}
+	}
+
+	var wtg sync.WaitGroup
+
+	wtg.Add(len(targeted_providers))
+
+	for _, tenantName := range targeted_providers {
+		fmt.Printf("Working with tenant %s\n", tenantName)
+		go analyzeTenant(&wtg, tenantName)
+	}
+
+	wtg.Wait()
+}
+
+func analyzeTenant(group *sync.WaitGroup, theTenant string) error {
+	defer group.Done()
+
+	service, err := providers.GetService(theTenant)
+	if err != nil {
+		log.Warnf("Unable to get service for tenant '%s': %s", theTenant, err.Error())
+		return err
+	}
+
+	templates, err := service.ListTemplates(true)
+
+	img, err := service.SearchImage("Ubuntu 16.04")
+	if err != nil {
+		log.Warnf("No image here...")
+		return err
+	}
+
+	// Prepare network
+
+	there := true
+	var net *api.Network = nil
+
+	netName := "scanner"
+	if net, err = service.GetNetwork(netName); net != nil && err == nil {
+		there = true
+		log.Warnf("Network '%s' already there", netName)
+	} else {
+		there = false
+	}
+
+	if !there {
+		net, err = service.CreateNetwork(api.NetworkRequest{
+			CIDR:      "192.168.0.0/24",
+			IPVersion: IPVersion.IPv4,
+			Name:      netName,
+		})
+		if err == nil {
+			defer service.DeleteNetwork(net.ID)
+		} else {
+			return errors.Wrapf(err, "Error waiting for server ready: %v", err)
+		}
+		if net == nil {
+			return errors.Errorf("Failure creating network")
+		}
+	}
+
+	_ = os.MkdirAll(utils.AbsPathify("$HOME/.safescale/scanner"), 0777)
+
+	var wg sync.WaitGroup
+
+	concurrency := math.Min(4, float64(len(templates) / 2))
+	sem := make(chan bool, int(concurrency))
+
+	hostAnalysis := func(template api.HostTemplate) error {
+		defer wg.Done()
+		if net != nil {
+			log.Printf("Checking template %s\n", template.Name)
+
+			host, err := service.CreateHost(api.HostRequest{
+				Name:       "scanhost-" + template.Name,
+				PublicIP:   true,
+				ImageID:    img.ID,
+				TemplateID: template.ID,
+				NetworkIDs: []string{net.ID},
+			})
+
+			defer service.DeleteHost("scanhost-" + template.Name)
+			if err != nil {
+				log.Warnf("[%s] host %s: error creation: %v\n", template.Name, template.Name, err.Error())
+				return err
+			}
+
+			ssh, err := service.GetSSHConfig(host.ID)
+			if err != nil {
+				log.Warnf("[%s] host %s: error reading SSHConfig: %v\n", template.Name, template.Name, err.Error())
+				return err
+			}
+			nerr := ssh.WaitServerReady(2 * time.Minute)
+			if nerr != nil {
+				log.Warnf("Error waiting for server ready: %v", nerr)
+				return nerr
+			}
+			c, err := ssh.Command(cmd)
+			if err != nil {
+				log.Warnf("Problem creating ssh command: %v", err)
+				return err
+			}
+			_, cout, _, err := c.Run()
+			if err != nil {
+				log.Warnf("Problem running ssh command: %v", err)
+				return err
+			}
+
+			daCpu, err := createCPUInfo(cout)
+			if err != nil {
+				log.Warnf("Problem building cpu info: %v", err)
+				return err
+			}
+
+			daCpu.TemplateName = template.Name
+			daCpu.TemplateID = template.ID
+			daCpu.ImageID = img.ID
+			daCpu.ImageName = img.Name
+			daCpu.TenantName = theTenant
+			daCpu.LastUpdated = time.Now().Format(time.RFC850)
+
+			daOut, err := json.MarshalIndent(daCpu, "", "\t")
+			if err != nil {
+				return err
+			}
+
+			nerr = ioutil.WriteFile( utils.AbsPathify("$HOME/.safescale/scanner/" +theTenant+ "#" + template.Name + ".json"), daOut, 0666)
+			if nerr != nil {
+				log.Warnf("Error writing file: %v", nerr)
+				return nerr
+			}
+		} else {
+			return errors.New("No gateway network !")
+		}
+
+		return nil
+	}
+
+	wg.Add(len(templates))
+
+	for _, target := range templates {
+		sem <- true
+		go func(inner api.HostTemplate) {
+			defer func() { <-sem }()
+			err = hostAnalysis(inner)
+			if err != nil {
+				log.Warnf("Error running scanner: %+v", err)
+			}
+		}(target)
+	}
+
+	for i := 0; i < cap(sem); i++ {
+		sem <- true
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
 func main() {
-	Run()
+	RunScanner()
 }
