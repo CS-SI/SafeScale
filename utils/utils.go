e89f67d7073880a86261fab85cd838220246d5bd
Oscar <oscar.picas@gmail.com>
2018-11-09 17:03:52 +0100

    Changes in UT

--- utils/utils.go
+++ utils/utils.go
@@ -65,29 +65,6 @@ func AbsPathify(inPath string) string {
 	return ""
 }
 
-// TODO Remove this later
-func OriginalAbsPathify(inPath string) string {
-	if strings.HasPrefix(inPath, "$HOME") {
-		inPath = userHomeDir() + inPath[5:]
-	}
-
-	if strings.HasPrefix(inPath, "$") {
-		end := strings.Index(inPath, string(os.PathSeparator))
-		inPath = os.Getenv(inPath[1:end]) + inPath[end:]
-	}
-
-	if filepath.IsAbs(inPath) {
-		return filepath.Clean(inPath)
-	}
-
-	p, err := filepath.Abs(inPath)
-	if err == nil {
-		return filepath.Clean(p)
-	}
-
-	return ""
-}
-
 func userHomeDir() string {
 	if runtime.GOOS == "windows" {
 		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
