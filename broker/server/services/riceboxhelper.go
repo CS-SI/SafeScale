b32aabc2605b2697681f72d887c82914392bbd66
Armand Jouanneau <armand.jouanneau@c-s.fr>
2018-11-16 15:10:13 +0100

    More explicit error

--- broker/server/services/riceboxhelper.go
+++ broker/server/services/riceboxhelper.go
@@ -18,7 +18,6 @@ package services
 
 import (
 	"bytes"
-	"fmt"
 	"text/template"
 
 	"github.com/pkg/errors"
@@ -60,16 +59,15 @@ func getBoxContent(script string, data interface{}) (string, error) {
 	}
 
 	tplcmd := buffer.String()
-	fmt.Println(tplcmd)
+	//fmt.Fprintln(tplcmd)
 	return tplcmd, nil
 }
 
-// Execute the given script (embeded in a rice-box) wit the given data on the host identified by hostid
+// Execute the given script (embeded in a rice-box) with the given data on the host identified by hostid
 func exec(script string, data interface{}, hostid string, provider *providers.Service) error {
 	scriptCmd, err := getBoxContent(script, data)
 	if err != nil {
-		// TODO Use more explicit error
-		tbr := errors.Wrap(err, "")
+		tbr := errors.Wrap(err, "Unable to get the script string")
 		log.Errorf("%+v", tbr)
 		return tbr
 	}
@@ -77,22 +75,20 @@ func exec(script string, data interface{}, hostid string, provider *providers.Se
 	sshSvc := NewSSHService(provider)
 	ssh, err := sshSvc.GetConfig(hostid)
 	if err != nil {
-		// TODO Use more explicit error
-		tbr := errors.Wrap(err, "")
+		tbr := errors.Wrap(err, "Unable to fetch the SSHConfig from the host")
 		log.Errorf("%+v", tbr)
 		return tbr
 	}
 
 	cmd, err := ssh.SudoCommand(scriptCmd)
 	if err != nil {
-		// TODO Use more explicit error
-		tbr := errors.Wrap(err, "")
+		tbr := errors.Wrap(err, "Unable to convert the script string in a SSHCommand struct")
 		log.Errorf("%+v", tbr)
 		return tbr
 	}
 	_, err = cmd.Output()
 
-	tbr := errors.Wrap(err, "")
+	tbr := errors.Wrap(err, "Unable to execute the command as a root user on the host")
 	log.Printf("%+v", tbr)
 	return tbr
 }
