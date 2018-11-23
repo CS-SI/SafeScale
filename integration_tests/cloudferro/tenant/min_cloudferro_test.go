c7890568e1a33afc36903cc1804e8a02eacb2d4c
Oscar <oscar.picas@gmail.com>
2018-11-23 16:36:27 +0100

    CloudFerro UT

---
+++ integration_tests/cloudferro/tenant/min_cloudferro_test.go
@@ -0,0 +1,55 @@
+package tenant
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+)
+
+func Test_Env_Setup(t *testing.T) {
+	runOnlyInIntegrationTest("TEST_CLOUDFERRO")
+
+	brokerd_launched, err := isBrokerdLaunched()
+	if !brokerd_launched {
+		fmt.Println("This requires that you launch brokerd in background and set the tenant")
+		require.True(t, brokerd_launched)
+	}
+	require.Nil(t, err)
+
+	in_path, err := canBeRun("broker")
+	require.Nil(t, err)
+
+	require.True(t, brokerd_launched)
+	require.True(t, in_path)
+}
+
+func Test_Setup(t *testing.T) {
+	runOnlyInIntegrationTest("TEST_CLOUDFERRO")
+
+	brokerd_launched, err := isBrokerdLaunched()
+	if !brokerd_launched {
+		fmt.Println("This requires that you launch brokerd in background and set the tenant")
+		require.True(t, brokerd_launched)
+	}
+	require.Nil(t, err)
+
+	in_path, err := canBeRun("broker")
+	require.Nil(t, err)
+
+	require.True(t, brokerd_launched)
+	require.True(t, in_path)
+
+	out, err := getOutput("broker tenant list")
+	require.Nil(t, err)
+	require.True(t, len(out) > 0)
+
+	out, err = getOutput("broker tenant get")
+	if err != nil {
+		fmt.Println("This requires that you set the right tenant before launching the tests")
+		require.Nil(t, err)
+	}
+	require.True(t, len(out) > 0)
+	require.True(t, strings.Contains(out, "ferro"))
+}
+
