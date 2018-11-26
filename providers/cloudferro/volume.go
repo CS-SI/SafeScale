53343c569cc0da035e6f897e75c2cd8e67df4c87
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2018-11-26 18:22:20 +0100

    CloudFerro volume using protocol volumev2...

--- providers/cloudferro/volume.go
+++ providers/cloudferro/volume.go
@@ -18,22 +18,22 @@ package cloudferro
 
 import (
 	"fmt"
-	"github.com/CS-SI/SafeScale/providers/openstack"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 
+	gc "github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack/blockstorage/v2/volumes"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/volumeattach"
 	"github.com/gophercloud/gophercloud/pagination"
 
-	"github.com/CS-SI/SafeScale/providers/metadata"
 	"github.com/CS-SI/SafeScale/providers/model"
-	"github.com/CS-SI/SafeScale/providers/model/enums/VolumeProperty"
 	"github.com/CS-SI/SafeScale/providers/model/enums/VolumeSpeed"
 	"github.com/CS-SI/SafeScale/providers/model/enums/VolumeState"
-	propsv1 "github.com/CS-SI/SafeScale/providers/model/properties/v1"
+	"github.com/CS-SI/SafeScale/providers/openstack"
+	"github.com/CS-SI/SafeScale/utils/retry"
 )
 
 // toVolumeState converts a Volume status returned by the OpenStack driver into VolumeState enum
@@ -87,15 +87,12 @@ func (client *Client) getVolumeSpeed(vType string) VolumeSpeed.Enum {
 // - size is the size of the volume in GB
 // - volumeType is the type of volume to create, if volumeType is empty the driver use a default type
 func (client *Client) CreateVolume(request model.VolumeRequest) (*model.Volume, error) {
-	// Check if a volume already exists with the same name
-	volume, err := metadata.LoadVolume(client, request.Name)
+	volume, err := client.GetVolume(request.Name)
 	if err != nil {
-		log.Debugf("Error creating volume, loading volume metadata: %+v", err)
-		return nil, errors.Wrap(err, fmt.Sprintf("Error creating volume, loading volume metadata"))
+		return nil, err
 	}
 	if volume != nil {
-		log.Debugf("Error creating volume, volume not found: %+v", err)
-		return nil, errors.Wrap(err, fmt.Sprintf("Volume '%s' already exists", request.Name))
+		return nil, fmt.Errorf("volume '%s' already exists", request.Name)
 	}
 
 	vol, err := volumes.Create(client.Volume, volumes.CreateOpts{
@@ -114,48 +111,34 @@ func (client *Client) CreateVolume(request model.VolumeRequest) (*model.Volume, 
 		Speed: client.getVolumeSpeed(vol.VolumeType),
 		State: toVolumeState(vol.Status),
 	}
-	err = metadata.SaveVolume(client, &v)
-	if err != nil {
-		nerr := client.DeleteVolume(v.ID)
-		if nerr != nil {
-			log.Warnf("Error deleting volume: %v", nerr)
-		}
-		log.Debugf("Error creating volume: saving volume metadata: %+v", err)
-		return nil, errors.Wrap(err, fmt.Sprintf("Error creating volume : %s", openstack.ProviderErrorToString(err)))
-	}
-
 	return &v, nil
 }
 
 // GetVolume returns the volume identified by id
 func (client *Client) GetVolume(id string) (*model.Volume, error) {
-	vol, err := volumes.Get(client.Volume, id).Extract()
+	r := volumes.Get(client.Volume, id)
+	volume, err := r.Extract()
 	if err != nil {
+		switch err.(type) {
+		case gc.ErrDefault404:
+			return nil, nil
+		}
 		log.Debugf("Error getting volume: getting volume invocation: %+v", err)
 		return nil, errors.Wrap(err, fmt.Sprintf("Error getting volume: %s", openstack.ProviderErrorToString(err)))
 	}
+
 	av := model.Volume{
-		ID:    vol.ID,
-		Name:  vol.Name,
-		Size:  vol.Size,
-		Speed: client.getVolumeSpeed(vol.VolumeType),
-		State: toVolumeState(vol.Status),
+		ID:    volume.ID,
+		Name:  volume.Name,
+		Size:  volume.Size,
+		Speed: client.getVolumeSpeed(volume.VolumeType),
+		State: toVolumeState(volume.Status),
 	}
 	return &av, nil
 }
 
-// ListVolumes return the list of all volume known on the current tenant (all=ture)
-//or 'only' thode monitored by safescale (all=false) ie those monitored by metadata
-func (client *Client) ListVolumes(all bool) ([]model.Volume, error) {
-	if all {
-		return client.listAllVolumes()
-	}
-	return client.listMonitoredVolumes()
-
-}
-
-// listAllVolumes list available volumes
-func (client *Client) listAllVolumes() ([]model.Volume, error) {
+// ListVolumes returns the list of all volumes known on the current tenant
+func (client *Client) ListVolumes() ([]model.Volume, error) {
 	var vs []model.Volume
 	err := volumes.List(client.Volume, volumes.ListOpts{}).EachPage(func(page pagination.Page) (bool, error) {
 		list, err := volumes.ExtractVolumes(page)
@@ -185,44 +168,38 @@ func (client *Client) listAllVolumes() ([]model.Volume, error) {
 	return vs, nil
 }
 
-// listMonitoredVolumes lists available Volumes created by SafeScale (ie registered in object storage)
-func (client *Client) listMonitoredVolumes() ([]model.Volume, error) {
-	var vols []model.Volume
-	m := metadata.NewVolume(client)
-	err := m.Browse(func(vol *model.Volume) error {
-		vols = append(vols, *vol)
-		return nil
-	})
-	if err != nil {
-		log.Debugf("Error listing monitored volumes: browsing volumes: %+v", err)
-		return nil, errors.Wrap(err, fmt.Sprintf("Error listing volumes : %s", openstack.ProviderErrorToString(err)))
-	}
-	return vols, nil
-}
-
 // DeleteVolume deletes the volume identified by id
 func (client *Client) DeleteVolume(id string) error {
-	volume, err := metadata.LoadVolume(client, id)
-	if err != nil {
-		log.Debugf("Error deleting volume: loading metadata: %+v", err)
-		return errors.Wrap(err, "Error deleting volume: loading metadata")
-	}
-	if volume == nil {
-		log.Debugf("Error deleting volume: volume not found: %+v", err)
-		return model.ResourceNotFoundError("volume", id)
-	}
+	var (
+		err     error
+		timeout = 30 * time.Second
+	)
 
-	r := volumes.Delete(client.Volume, id, nil)
-	err = r.ExtractErr()
-	if err != nil {
-		spew.Dump(r.Err)
-		log.Debugf("Error deleting volume: actual delete call: %+v", err)
-		return errors.Wrap(err, fmt.Sprintf("Error deleting volume: %s", openstack.ProviderErrorToString(err)))
-	}
-	err = metadata.RemoveVolume(client, id)
-	if err != nil {
-		log.Debugf("Error deleting volume: removing volume metadata: %+v", err)
-		return errors.Wrap(err, fmt.Sprintf("Error deleting volume: %s", openstack.ProviderErrorToString(err)))
+	retryErr := retry.WhileUnsuccessfulDelay5Seconds(
+		func() error {
+			r := volumes.Delete(client.Client.Volume, id, nil)
+			err := r.ExtractErr()
+			if err != nil {
+				switch err.(type) {
+				case gc.ErrDefault400:
+					return fmt.Errorf("volume not in state 'available'")
+				default:
+					return err
+				}
+			}
+			return nil
+		},
+		timeout,
+	)
+	if retryErr != nil {
+		switch retryErr.(type) {
+		case retry.ErrTimeout:
+			if err != nil {
+				return fmt.Errorf("timeout after %v to delete volume: %v", timeout, err)
+			}
+		}
+		log.Debugf("Error deleting volume: %+v", retryErr)
+		return errors.Wrap(retryErr, fmt.Sprintf("Error deleting volume: %v", retryErr))
 	}
 	return nil
 }
@@ -232,34 +209,6 @@ func (client *Client) DeleteVolume(id string) error {
 // - 'volume' to attach
 // - 'host' on which the volume is attached
 func (client *Client) CreateVolumeAttachment(request model.VolumeAttachmentRequest) (string, error) {
-	// Ensure volume and host are known
-	mv, err := metadata.LoadVolume(client, request.VolumeID)
-	if err != nil {
-		return "", err
-	}
-	if mv == nil {
-		return "", errors.Wrap(model.ResourceNotFoundError("volume", request.VolumeID), "Cannot create volume attachment")
-	}
-	volume := mv.Get()
-
-	volumeAttachedV1 := propsv1.NewVolumeAttachments()
-	err = volume.Properties.Get(VolumeProperty.AttachedV1, volumeAttachedV1)
-	if err != nil {
-		return "", err
-	}
-	if len(volumeAttachedV1.HostIDs) == 1 {
-		// For now, allows only one attachment...
-		return "", fmt.Errorf("Volume '%s' already attached to host(s)", request.VolumeID)
-	}
-
-	// Loads host
-	mh, err := metadata.LoadHost(client, request.HostID)
-	if err != nil {
-		return "", err
-	}
-	if mh == nil {
-		return "", errors.Wrap(model.ResourceNotFoundError("host", request.HostID), "Cannot create volume attachment")
-	}
 
 	// Creates the attachment
 	r := volumeattach.Create(client.Compute, request.HostID, volumeattach.CreateOpts{
@@ -323,30 +272,13 @@ func (client *Client) ListVolumeAttachments(serverID string) ([]model.VolumeAtta
 }
 
 // DeleteVolumeAttachment deletes the volume attachment identifed by id
-func (client *Client) DeleteVolumeAttachment(serverID, id string) error {
-	va, err := client.GetVolumeAttachment(serverID, id)
+func (client *Client) DeleteVolumeAttachment(serverID, vaID string) error {
+	r := volumeattach.Delete(client.Compute, serverID, vaID)
+	err := r.ExtractErr()
 	if err != nil {
-		log.Debugf("Error deleting volume attachment: getting attachments: %+v", err)
-		return errors.Wrap(err, fmt.Sprintf("Error deleting volume attachment %s: %s", id, openstack.ProviderErrorToString(err)))
-	}
-
-	err = volumeattach.Delete(client.Compute, serverID, id).ExtractErr()
-	if err != nil {
+		spew.Dump(r)
 		log.Debugf("Error deleting volume attachment: deleting attachments: %+v", err)
-		return errors.Wrap(err, fmt.Sprintf("Error deleting volume attachment %s: %s", id, openstack.ProviderErrorToString(err)))
+		return errors.Wrap(err, fmt.Sprintf("Error deleting volume attachment '%s': %s", vaID, openstack.ProviderErrorToString(err)))
 	}
-
-	mtdVol, err := metadata.LoadVolume(client, id)
-	if err != nil {
-		log.Debugf("Error deleting volume attachment: loading metadata: %+v", err)
-		return errors.Wrap(err, fmt.Sprintf("Error deleting volume attachment %s: %s", id, openstack.ProviderErrorToString(err)))
-	}
-
-	err = mtdVol.Detach(va)
-	if err != nil {
-		log.Debugf("Error deleting volume attachment: detaching volume: %+v", err)
-		return errors.Wrap(err, fmt.Sprintf("Error deleting volume attachment %s: %s", id, openstack.ProviderErrorToString(err)))
-	}
-
 	return nil
 }
