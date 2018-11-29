2663a03c353e041718b726ec7995ef873bdd7531
Oscar <oscar.picas@gmail.com>
2018-11-29 12:02:45 +0100

    Fixed OVH integration tests

--- integration_tests/validation_ovh_test.go
+++ integration_tests/validation_ovh_test.go
@@ -164,8 +164,8 @@ func Test_Basic(t *testing.T) {
 	require.True(t, strings.Contains(out, "deleted"))
 
 	out, err = getOutput("broker host delete gw-crazy")
-	require.Nil(t, err)
-	require.True(t, strings.Contains(out, "deleted"))
+	require.NotNil(t, err)
+	require.True(t, strings.Contains(out, "gateway"))
 
 	out, err = getOutput("broker network delete crazy")
 	require.Nil(t, err)
