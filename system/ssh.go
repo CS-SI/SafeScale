43f514bac6b66189b22503e1937c4b8ea5c4e8cf
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2018-12-04 18:21:26 +0100

    Enhancements in userdata
    -  DNS management for RedHat/CentOS in userdata has been revisited due to some recent issue
    - In sudoers, requiretty is disabled for admin user (may prevent the correct execution of userdata finalization)

--- system/ssh.go
+++ system/ssh.go
@@ -567,12 +567,12 @@ func (ssh *SSHConfig) WaitServerReady(timeout time.Duration) error {
 		retcode, stdout, stderr, logErr := logCmd.Run()
 		if logErr == nil {
 			if retcode == 0 {
-				return fmt.Errorf("server '%s' is not ready yet : %s, Log content of file user_data.log: %s", ssh.Host, originalErr.Error(), stdout)
+				return fmt.Errorf("server '%s' is not ready yet: %s - log content of file user_data.log: %s", ssh.Host, originalErr.Error(), stdout)
 			}
-			return fmt.Errorf("server '%s' is not ready yet : %s, Error reading user_data.log: %s", ssh.Host, originalErr.Error(), stderr)
+			return fmt.Errorf("server '%s' is not ready yet: %s - error reading user_data.log: %s", ssh.Host, originalErr.Error(), stderr)
 		}
 
-		return fmt.Errorf("server '%s' is not ready yet : %s", ssh.Host, originalErr.Error())
+		return fmt.Errorf("server '%s' is not ready yet: %s", ssh.Host, originalErr.Error())
 	}
 	return nil
 }
