e06578158437fc70cb6aab4f1e40e0a758048a70
Oscar <oscar.picas@gmail.com>
2018-12-14 14:40:19 +0100

    Changes updating scanner db

--- scanner/collect.go
+++ scanner/collect.go
@@ -16,24 +16,7 @@ import (
 // StoredCPUInfo ...
 type StoredCPUInfo struct {
 	Id           string `bow:"key"`
-	TenantName   string `json:"tenant_name,omitempty"`
-	TemplateID   string `json:"template_id,omitempty"`
-	TemplateName string `json:"template_name,omitempty"`
-	ImageID      string `json:"image_id,omitempty"`
-	ImageName    string `json:"image_name,omitempty"`
-	LastUpdated  string `json:"last_updated,omitempty"`
-
-	NumberOfCPU    int     `json:"number_of_cpu,omitempty"`
-	NumberOfCore   int     `json:"number_of_core,omitempty"`
-	NumberOfSocket int     `json:"number_of_socket,omitempty"`
-	CPUFrequency   float64 `json:"cpu_frequency,omitempty"`
-	CPUArch        string  `json:"cpu_arch,omitempty"`
-	Hypervisor     string  `json:"hypervisor,omitempty"`
-	CPUModel       string  `json:"cpu_model,omitempty"`
-	RAMSize        float64 `json:"ram_size,omitempty"`
-	RAMFreq        float64 `json:"ram_freq,omitempty"`
-	GPU            int     `json:"gpu,omitempty"`
-	GPUModel       string  `json:"gpu_model,omitempty"`
+	CPUInfo
 }
 
 func collect() {
