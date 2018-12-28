f6909746009c86eb4b1679f784f13a6c13416d14
Armand Jouanneau <armand.jouanneau@c-s.fr>
2018-12-28 13:49:08 +0100

    Integration Test Refactor
    Use generic tests for each provider

--- integration_tests/cloudferro/tenant/min_cloudferro_test.go
+++ integration_tests/cloudferro/tenant/min_cloudferro_test.go
@@ -1,56 +1,16 @@
 package tenant
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/CS-SI/SafeScale/integration_tests"
-	"github.com/stretchr/testify/require"
+	"github.com/CS-SI/SafeScale/integration_tests/enums/Providers"
 )
 
 func Test_Env_Setup(t *testing.T) {
-	integration_tests.RunOnlyInIntegrationTest("TEST_CLOUDFERRO")
-
-	brokerd_launched, err := integration_tests.IsBrokerdLaunched()
-	if !brokerd_launched {
-		fmt.Println("This requires that you launch brokerd in background and set the tenant")
-		require.True(t, brokerd_launched)
-	}
-	require.Nil(t, err)
-
-	in_path, err := integration_tests.CanBeRun("broker")
-	require.Nil(t, err)
-
-	require.True(t, brokerd_launched)
-	require.True(t, in_path)
+	integration_tests.EnvSetup(t, Providers.CLOUDFERRO)
 }
 
 func Test_Setup(t *testing.T) {
-	integration_tests.RunOnlyInIntegrationTest("TEST_CLOUDFERRO")
-
-	brokerd_launched, err := integration_tests.IsBrokerdLaunched()
-	if !brokerd_launched {
-		fmt.Println("This requires that you launch brokerd in background and set the tenant")
-		require.True(t, brokerd_launched)
-	}
-	require.Nil(t, err)
-
-	in_path, err := integration_tests.CanBeRun("broker")
-	require.Nil(t, err)
-
-	require.True(t, brokerd_launched)
-	require.True(t, in_path)
-
-	out, err := integration_tests.GetOutput("broker tenant list")
-	require.Nil(t, err)
-	require.True(t, len(out) > 0)
-
-	out, err = integration_tests.GetOutput("broker tenant get")
-	if err != nil {
-		fmt.Println("This requires that you set the right tenant before launching the tests")
-		require.Nil(t, err)
-	}
-	require.True(t, len(out) > 0)
-	require.True(t, strings.Contains(out, "ferro"))
+	integration_tests.Setup(t, Providers.CLOUDFERRO)
 }
--- integration_tests/ovh/init/min_ovh_test.go
+++ integration_tests/ovh/init/min_ovh_test.go
@@ -1,26 +1,12 @@
 package init
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/CS-SI/SafeScale/integration_tests"
-	"github.com/stretchr/testify/require"
+	"github.com/CS-SI/SafeScale/integration_tests/enums/Providers"
 )
 
 func Test_Env_Setup(t *testing.T) {
-	integration_tests.RunOnlyInIntegrationTest("TEST_OVH")
-
-	brokerd_launched, err := integration_tests.IsBrokerdLaunched()
-	if !brokerd_launched {
-		fmt.Println("This requires that you launch brokerd in background and set the tenant")
-		require.True(t, brokerd_launched)
-	}
-	require.Nil(t, err)
-
-	in_path, err := integration_tests.CanBeRun("broker")
-	require.Nil(t, err)
-
-	require.True(t, brokerd_launched)
-	require.True(t, in_path)
+	integration_tests.EnvSetup(t, Providers.OVH)
 }
