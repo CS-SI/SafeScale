74e738d6f74136978568c95f71c99194f3b70b39
Armand Jouanneau <armand.jouanneau@c-s.fr>
2018-12-28 13:49:36 +0100

    Integration Tests for local driver
    WIP

---
+++ integration_tests/local/tenant/min_local_test.go
@@ -0,0 +1,16 @@
+package tenant
+
+import (
+	"testing"
+
+	"github.com/CS-SI/SafeScale/integration_tests"
+	"github.com/CS-SI/SafeScale/integration_tests/enums/Providers"
+)
+
+func Test_Env_Setup(t *testing.T) {
+	integration_tests.EnvSetup(t, Providers.LOCAL)
+}
+
+func Test_Setup(t *testing.T) {
+	integration_tests.Setup(t, Providers.LOCAL)
+}
