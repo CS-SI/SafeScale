e9673d591fa8600c52e3664ca2ac3f1a8e20d39f
Armand Jouanneau <armand.jouanneau@c-s.fr>
2019-01-08 17:03:44 +0100

    Added validation test for the reverse proxy feature

--- integration_tests/feature_tests.go
+++ integration_tests/feature_tests.go
@@ -1,6 +1,7 @@
 package integration_tests
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 	"testing"
@@ -65,7 +66,7 @@ func DockerNotGateway(t *testing.T, provider Providers.Enum) {
 	require.Nil(t, err)
 	require.True(t, strings.Contains(out, "0 users"))
 
-	out, err = GetOutput("broker host create " + names.Hosts[0] + " --public --net " + names.Networks[0])
+	out, err = GetOutput("broker host create " + names.Hosts[0] + " --net " + names.Networks[0])
 	require.Nil(t, err)
 
 	out, err = GetOutput("deploy host check-feature " + names.Hosts[0] + " docker")
@@ -129,6 +130,82 @@ func DockerCompose(t *testing.T, provider Providers.Enum) {
 	require.NotNil(t, err)
 
 	out, err = GetOutput("broker ssh run gw-" + names.Networks[0] + " -c \"docker-compose -v\"")
+	fmt.Print(out)
+	require.NotNil(t, err)
+}
+
+func RemoteDesktop(t *testing.T, provider Providers.Enum) {
+	Setup(t, provider)
+
+	names := GetNames("RemoteDesktop", 0, 0, 0, 1, 1, 0)
+	names.TearDown()
+	defer names.TearDown()
+
+	out, err := GetOutput("broker network create " + names.Networks[0] + " --cidr 168.192.103.0/24")
+	require.Nil(t, err)
+
+	out, err = GetOutput("broker host create " + names.Hosts[0] + " --public --net " + names.Networks[0])
+	require.Nil(t, err)
+	host := HostInfo{}
+	json.Unmarshal([]byte(out), &host)
+
+	out, err = GetOutput("deploy host check-feature --param Password=SafeScale " + names.Hosts[0] + " remotedesktop")
+	require.NotNil(t, err)
+
+	out, err = GetOutput("deploy host add-feature --skip-proxy --param Password=SafeScale " + names.Hosts[0] + " remotedesktop")
+	require.Nil(t, err)
+
+	//TODO : try to connect to the host through guacamole?
+	out, err = GetOutput("wget " + host.PUBLIC_IP + ":9080/guacamole")
+	fmt.Print(out)
+	require.Nil(t, err)
+
+	fmt.Println(names.Hosts[0])
+	out, err = GetOutput("deploy host check-feature --param Password=SafeScale " + names.Hosts[0] + " remotedesktop")
+	fmt.Println(out)
+	require.Nil(t, err)
+
+	out, err = GetOutput("deploy host delete-feature --param Password=SafeScale " + names.Hosts[0] + " remotedesktop")
+	require.Nil(t, err)
+
+	out, err = GetOutput("deploy host check-feature --param Password=SafeScale " + names.Hosts[0] + " remotedesktop")
+	require.NotNil(t, err)
+
+	out, err = GetOutput("wget " + host.PUBLIC_IP + ":9080/guacamole")
+	fmt.Print(out)
+	require.NotNil(t, err)
+}
+
+func ReverseProxy(t *testing.T, provider Providers.Enum) {
+	Setup(t, provider)
+
+	names := GetNames("ReverseProxy", 0, 0, 0, 0, 1, 0)
+	names.TearDown()
+	defer names.TearDown()
+
+	out, err := GetOutput("broker network create " + names.Networks[0] + " --cidr 168.192.104.0/24")
+	require.Nil(t, err)
+
+	out, err = GetOutput("deploy host check-feature gw-" + names.Networks[0] + " reverseproxy")
+	require.NotNil(t, err)
+
+	out, err = GetOutput("deploy host add-feature gw-" + names.Networks[0] + " reverseproxy")
+	require.Nil(t, err)
+
+	out, err = GetOutput("broker ssh run gw-" + names.Networks[0] + " -c \"curl -Ssl -I -k https://localhost:8444/ 2>&1 | grep \\\"HTTP/1.1 200 OK\\\"\"")
+	fmt.Print(out)
+	require.Nil(t, err)
+
+	out, err = GetOutput("deploy host check-feature gw-" + names.Networks[0] + " reverseproxy")
+	require.Nil(t, err)
+
+	out, err = GetOutput("deploy host delete-feature gw-" + names.Networks[0] + " reverseproxy")
+	require.Nil(t, err)
+
+	out, err = GetOutput("deploy host check-feature gw-" + names.Networks[0] + " reverseproxy")
+	require.NotNil(t, err)
+
+	out, err = GetOutput("broker ssh run gw-" + names.Networks[0] + " -c \"curl -Ssl -I -k https://localhost:8444/ 2>&1 | grep \\\"HTTP/1.1 200 OK\\\"\"")
 	fmt.Print(out)
 	require.NotNil(t, err)
 }
