4a1d2b69d01ac7ec6a150088eb19d2e63c13f7f3
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2019-06-13 15:44:20 +0200

    Introduced data package
    - Map struct to carry map related common functions
    - Clonable interface to drive data that can be clonable
    - Identifyable interface to drive data that can be identified by name or by id
    - Serializable interface to drive data that can be (de)serialized

---
+++ lib/utils/data/clonable.go
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018-2019, CS Systemes d'Information, http://www.c-s.fr
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package data
+
+// Clonable is the interface a struct must satisfy to be able to be cloned
+type Clonable interface {
+	// Clone allows to duplicate Clonable
+	Clone() Clonable
+	// Replace allows to replace a Clonable with data from another one
+	Replace(Clonable) Clonable
+	// // Content returns the content of the Clonable
+	// Content() interface{}
+}
