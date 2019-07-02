b20f38c68fcc7c98a79a91782fde6771b6937e05
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2019-07-02 12:40:30 +0200

    Refined sizings for cluster flavors
    - makes sure not GPU is wanted by default (GpuCount = -1)
    - reduces RAM requirements for gateway

--- lib/server/cluster/flavors/k8s/k8s.go
+++ lib/server/cluster/flavors/k8s/k8s.go
@@ -79,9 +79,10 @@ func gatewaySizing(task concurrency.Task, foreman control.Foreman) pb.HostDefini
 		Sizing: &pb.HostSizing{
 			MinCpuCount: 2,
 			MaxCpuCount: 4,
-			MinRamSize:  15.0,
-			MaxRamSize:  32.0,
+			MinRamSize:  7.0,
+			MaxRamSize:  16.0,
 			MinDiskSize: 50,
+			GpuCount:    -1,
 		},
 	}
 }
@@ -94,6 +95,7 @@ func nodeSizing(task concurrency.Task, foreman control.Foreman) pb.HostDefinitio
 			MinRamSize:  15.0,
 			MaxRamSize:  32.0,
 			MinDiskSize: 80,
+			GpuCount:    -1,
 		},
 	}
 }
