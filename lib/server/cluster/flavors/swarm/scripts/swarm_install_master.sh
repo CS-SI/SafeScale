19f8ec6f09ecd2f4b9b0636e0792d0e13c49951a
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2019-07-20 11:16:09 +0200

    Install SafeScale binaries and tenants.json file

--- lib/server/cluster/flavors/swarm/scripts/swarm_install_master.sh
+++ lib/server/cluster/flavors/swarm/scripts/swarm_install_master.sh
@@ -28,5 +28,22 @@ exec 2>&1
 # Installs and configures everything needed on any node
 {{ .reserved_CommonRequirements }}
 
+# Installs safescale binaries
+LIST=$(curl -s https://api.github.com/repos/CS-SI/Safescale/releases/latest | grep browser_download_url | grep safescale | cut -d'"' -f4)
+cd /usr/local/bin
+for i in $LIST; do
+    sfDownload "$i" "$(basename $i)" 5m 5 || exit 192
+done
+mv safescaled-Linux-x86_64.bin safescaled
+mv safescale-Linux-x86_64.bin safescale
+chown root:root safescale*
+chmod u+rwx,go+rx-w safescale*
+
+# Set tenant.json file
+mkdir -p /etc/safescale
+cat >/etc/safescale/tenants.json <<-'EOF'
+{{ .reserved_TenantJSON }}
+EOF
+
 echo "Master installed successfully."
 exit 0
