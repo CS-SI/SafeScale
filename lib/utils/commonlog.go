262e49953bbc3a4d79dc8d173fe99e2dd430d3e9
Oscar <oscar.picas@gmail.com>
2019-09-04 14:56:42 +0200

    Changes in log format

--- lib/utils/commonlog.go
+++ lib/utils/commonlog.go
@@ -20,6 +20,16 @@ type MyFormatter struct {
 	pid string
 }
 
+func GetDefaultFormatter() *MyFormatter {
+	return &MyFormatter{
+		TextFormatter: logrus.TextFormatter{
+			ForceColors:            true,
+			TimestampFormat:        "2006-01-02 15:04:05.000",
+			FullTimestamp:          true,
+			DisableLevelTruncation: true,
+		}}
+}
+
 // Format ...
 func (f *MyFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	if f.TextFormatter.DisableLevelTruncation && f.TextFormatter.ForceColors {
@@ -29,7 +39,9 @@ func (f *MyFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 		}
 		bc, err := f.TextFormatter.Format(entry)
 		ticket := string(bc)
-		replaced := strings.Replace(ticket, "[20", ""+strings.Repeat(" ", 8-len(entry.Level.String()))+"["+f.pid+"][20", 1)
+		// replaced := strings.Replace(ticket, "[20", ""+strings.Repeat(" ", 8-len(entry.Level.String()))+"[" + f.pid + "][20", 1)
+		replaced := strings.Replace(ticket, "[20", ""+strings.Repeat(" ", 8-len(entry.Level.String()))+"[20", 1)
+		replaced = strings.Replace(replaced, "] ", "]["+entry.Level.String()+"]["+f.pid+"] ", 1)
 
 		return []byte(replaced), err
 	}
