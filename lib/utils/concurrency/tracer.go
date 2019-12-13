a5ea546f151575cd80fdf0911479420146d0ff2a
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2019-12-13 14:59:50 +0100

    Merged feature k8s.helm2 in kubernetes
    + code cleanup

--- lib/utils/concurrency/tracer.go
+++ lib/utils/concurrency/tracer.go
@@ -41,7 +41,7 @@ type Tracer struct {
 	sw           temporal.Stopwatch
 }
 
-// IsLogActive ...
+// IsLogActive ... FIXME
 func IsLogActive(key string) bool {
 	if logs := os.Getenv("SAFESCALE_OPTIONAL_LOGS"); logs != "" {
 		return strings.Contains(logs, key)
