2bf3483f3489df6926be7b8f441781dfe1fbf6e1
Oscar <oscar.picas@gmail.com>
2020-01-14 14:56:18 +0100

    Changes in style target

--- common.mk
+++ common.mk
@@ -1,4 +1,4 @@
-VERSION=19.11.0-alpha
+VERSION=20.1.0-alpha
 export VERSION
 
 ifeq ($(MAKE_LEVEL),)
@@ -23,6 +23,7 @@ REMOTE := $(shell git rev-parse $(UPSTREAM))
 BASE := $(shell git merge-base HEAD $(UPSTREAM))
 
 GO?=go
+GOFMT?=gofmt
 CP?=cp
 RM?=rm
 BROWSER?=firefox
