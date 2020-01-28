67ce7b1eb7b814437a5658d82fc97065944c6a18
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2020-01-28 17:04:15 +0100

    Updated documentation

--- lib/server/cluster/control/controller.go
+++ lib/server/cluster/control/controller.go
@@ -1085,7 +1085,7 @@ func (c *Controller) deleteNode(task concurrency.Task, node *clusterpropsv1.Node
 	err = client.New().Host.Delete([]string{node.ID}, temporal.GetLongOperationTimeout())
 	if err != nil {
 		if _, ok := err.(scerr.ErrNotFound); ok {
-			// host seems already deleted, so it's a success (handles the case where )
+			// host seems already deleted, so it's a success :-)
 			return nil
 		}
 		return err
