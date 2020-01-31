e260e0eb31936d66c972e21e027a7f0a46728ca4
Oscar <oscar.picas@gmail.com>
2020-01-31 15:00:59 +0100

    Changes in build and docker generation

--- build/create-docker.sh
+++ build/create-docker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 WRKDIR=$(readlink -f $(dirname "$0"))
+date > marker
 docker build --rm --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f ${WRKDIR}/Dockerfile -t safescale:latest ${WRKDIR}
