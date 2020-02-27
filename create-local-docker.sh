e58aed5f716dbe51105982d71bc88f0c567116d0
Oscar <oscar.picas@gmail.com>
2020-02-27 12:05:21 +0100

    Build with docker

---
+++ create-local-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+WRKDIR=$(readlink -f $(dirname "$0"))
+
+if [ ! -f ./marker ]; then
+    date > ./marker
+fi
+
+docker build --rm --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f ${WRKDIR}/Dockerfile.local -t safescale:issues ${WRKDIR}
+[ $? -ne 0 ] && echo "Docker build failed !!" && rm -f ./marker && return 1
+
+echo "Docker build OK"
+
+docker create -ti --name dummy safescale:issues bash
+[ $? -ne 0 ] && echo "Failure extracting binaries 1/3" && return 1
+docker cp dummy:/exported .
+[ $? -ne 0 ] && echo "Failure extracting binaries 2/3" && return 1
+docker rm -f dummy
+[ $? -ne 0 ] && echo "Failure extracting binaries 3/3" && return 1
+
+echo "Binaries extracted successfully"
+return 0
