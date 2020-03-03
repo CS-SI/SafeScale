7f9135fc64cf0784fca4698d84aa1692915e9764
Oscar <oscar.picas@gmail.com>
2020-03-03 17:54:21 +0100

    Added UT to detect goroutine leaks

--- lib/utils/concurrency/task_test.go
+++ lib/utils/concurrency/task_test.go
@@ -1,14 +1,20 @@
-
 package concurrency
 
 import (
 	"fmt"
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 	"time"
 )
 
 func TestDoesAbortReallyAbortOrIsJustFakeNews(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
 	single, err := NewTask(nil)
 	require.NotNil(t, single)
 	require.Nil(t, err)
@@ -20,12 +26,11 @@ func TestDoesAbortReallyAbortOrIsJustFakeNews(t *testing.T) {
 				forever = false
 				break
 			}
-			time.Sleep(time.Duration(90) * time.Millisecond)
 			fmt.Println("Forever young...")
-
+			time.Sleep(time.Duration(90) * time.Millisecond)
 		}
 		return "I want to be forever young", nil
-	}, nil, time.Duration(4000) * time.Millisecond)
+	}, nil, time.Duration(4000)*time.Millisecond)
 	require.Nil(t, err)
 
 	time.Sleep(time.Duration(2000) * time.Millisecond)
@@ -38,15 +43,83 @@ func TestDoesAbortReallyAbortOrIsJustFakeNews(t *testing.T) {
 	time.Sleep(time.Duration(2000) * time.Millisecond)
 
 	require.Nil(t, err)
+
+	_ = w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	// Here, last 2 lines of the output should be:
+	// Forever young...
+	// Aborted
+
+	outString := string(out)
+	nah := strings.Split(outString, "\n")
+
+	if !strings.Contains(nah[len(nah)-2], "Aborted") {
+		t.Fail()
+	}
 }
 
+func TestDoesItLeakWhenTimeouts(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
 
-func TestAbortButThisTimeUsingTrueAbortChannel(t *testing.T) {
 	single, err := NewTask(nil)
 	require.NotNil(t, single)
 	require.Nil(t, err)
 
-	trueAbort := make(chan struct{}) // TODO trueAbort dans TaskParameters ?
+	single, err = single.StartWithTimeout(func(t Task, parameters TaskParameters) (result TaskResult, err error) {
+		forever := true
+		for forever {
+			if t.Aborted() {
+				forever = false
+				break
+			}
+			fmt.Println("Forever young...")
+			time.Sleep(time.Duration(10) * time.Millisecond)
+		}
+		return "I want to be forever young", nil
+	}, nil, time.Duration(100)*time.Millisecond)
+	require.Nil(t, err)
+
+	time.Sleep(time.Duration(200) * time.Millisecond)
+	// by now single should succeed
+	err = single.Abort()
+
+	fmt.Println("Aborted")
+
+	// Nothing wrong should happen after this point...
+	time.Sleep(time.Duration(1000) * time.Millisecond)
+
+	require.Nil(t, err)
+
+	_ = w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	// Here, last 2 lines of the output should be:
+	// Forever young...
+	// Aborted
+
+	outString := string(out)
+	nah := strings.Split(outString, "\n")
+
+	if !strings.Contains(nah[len(nah)-2], "Aborted") {
+		t.Fail()
+	}
+}
+
+func TestAbortButThisTimeUsingTrueAbortChannel(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	single, err := NewTask(nil)
+	require.NotNil(t, single)
+	require.Nil(t, err)
+
+	trueAbort := make(chan struct{})
 	single, err = single.StartWithTimeout(func(t Task, parameters TaskParameters) (result TaskResult, err error) {
 		forever := true
 		for forever {
@@ -56,22 +129,44 @@ func TestAbortButThisTimeUsingTrueAbortChannel(t *testing.T) {
 				forever = false
 				break
 			default:
-				time.Sleep(time.Duration(90) * time.Millisecond)
 				fmt.Println("Forever young...")
+				time.Sleep(time.Duration(10) * time.Millisecond)
 			}
 		}
 		return "I want to be forever young", nil
-	}, nil, time.Duration(4000) * time.Millisecond)
+	}, nil, time.Duration(40)*time.Millisecond)
 	require.Nil(t, err)
 
-	time.Sleep(time.Duration(2000) * time.Millisecond)
+	time.Sleep(time.Duration(200) * time.Millisecond)
 
 	err = single.Abort()
 	trueAbort <- struct{}{}
 
 	fmt.Println("Aborted")
 
-	time.Sleep(time.Duration(2000) * time.Millisecond)
-
 	require.Nil(t, err)
+
+	_ = w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	// Here, last 3 lines of the output should be:
+	// Forever young...
+	// I'm Gotham's reckoning. Here to end the borrowed time you all have been living on.
+	// Aborted
+
+	outString := string(out)
+	nah := strings.Split(outString, "\n")
+
+	if !strings.Contains(nah[len(nah)-4], "Forever young") {
+		t.Fail()
+	}
+
+	if !strings.Contains(nah[len(nah)-3], "I'm Gotham's reckoning") {
+		t.Fail()
+	}
+
+	if !strings.Contains(nah[len(nah)-2], "Aborted") {
+		t.Fail()
+	}
 }
