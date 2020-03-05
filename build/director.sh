eaa19d497b53a0a18f508d8915a45603e7bdd345
Oscar <oscar.picas@gmail.com>
2020-03-05 16:18:21 +0100

    CI changes

--- build/director.sh
+++ build/director.sh
@@ -5,5 +5,4 @@ if [[ ! -v BUILD_ENV ]]; then
     echo "BUILD_ENV is not set, this script is intented to run inside a docker container" && return 1
 fi
 
-export TENANT=google
 ./small.sh
---
+++ build/small.sh
@@ -0,0 +1,119 @@
+#! /bin/bash -x
+
+whydied() {
+    ./safescale-cover ssh run -c "sudo zip -r /tmp/dump.zip /opt/safescale/" $1 || return 1
+    ./safescale-cover ssh copy $1:/tmp/dump.zip ~/.safescale/$2-$1-forensics.zip || return 2
+    ./safescale-cover ssh run -c "sudo rm -rf /tmp/dump.zip" $1 || return 3
+    return 0
+}
+
+whylives() {
+    ./safescale-cover ssh run -c "sudo zip -r /tmp/dump.zip /opt/safescale/" $1 || return 1
+    ./safescale-cover ssh copy $1:/tmp/dump.zip ~/.safescale/$2-$1-alive.zip || return 2
+    ./safescale-cover ssh run -c "sudo rm -rf /tmp/dump.zip" $1 || return 3
+    return 0
+}
+
+alert() {
+	notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$1"
+}
+
+settos() {
+    if [ -z "$1" ]
+    then
+        echo "No timeout specified"
+    else
+        export SAFESCALE_EXECUTION_TIMEOUT=$1
+        export SAFESCALE_HOST_TIMEOUT=$1
+        export SAFESCALE_HOST_CREATION_TIMEOUT=$1
+        export SAFESCALE_SSH_CONNECT_TIMEOUT=$1
+        export SAFESCALE_FORENSICS=True
+    fi
+}
+
+echo "Checks..."
+if [[ ! -v BUILD_ENV ]]; then
+    echo "BUILD_ENV is not set, this script is intented to run inside a docker container" && exit 1
+fi
+
+if [[ ! -v TENANT ]]; then
+    echo "TENANT is not set..." && exit 1
+fi
+
+settos 24m
+
+./safescaled-cover&
+
+sleep 3
+
+./safescale tenant set $TENANT
+
+sleep 3
+
+ROUNDS=5
+CODE=0
+RUN=0
+
+## declare an array variable
+declare -a flavor=("k8s" "boh" "swarm")
+
+for fla in "${flavor[@]}"
+do
+  RUN=0
+
+  for i in $(seq $ROUNDS); do
+    ./safescale-cover cluster delete clu-$TENANT-ubu-$fla-r$i -y
+    ./safescale-cover cluster create -k -F $fla --os "ubuntu 18.04" --sizing "cpu=2,ram>=2,disk>=8" --cidr 10.4.$i.0/24 clu-$TENANT-ubu-$fla-r$i
+    RUN=$?
+    if [[ $RUN -ne 0 ]]; then
+      CODE=$((CODE + 1))
+    fi
+    machines=$(./safescale host ls | tail -n 1 | jq '.result' | jq -r '.[] | .name')
+    stamp=`date +"%s"`
+    nmach=$(echo $machines | wc -w)
+    if [[ $nmach -ne 3 ]]; then
+      for machine in $machines
+      do
+          whydied $machine $stamp
+      done
+    else
+      for machine in $machines
+      do
+          whylives $machine $stamp
+      done
+    fi
+    ./safescale-cover cluster delete clu-$TENANT-ubu-$fla-r$i -y
+    if [[ $RUN -eq 0 ]]; then
+      break
+    fi
+  done
+  
+  for i in $(seq $ROUNDS); do
+    ./safescale-cover cluster delete clu-$TENANT-cent7-$fla-r$i -y
+    ./safescale-cover cluster create -k -F $fla --os "centos 7" --sizing "cpu=2,ram>=2,disk>=8" --cidr 10.7.$i.0/24 clu-$TENANT-cent7-$fla-r$i
+    RUN=$?
+    if [[ $RUN -ne 0 ]]; then
+      CODE=$((CODE + 1))
+    fi
+    machines=$(./safescale host ls | tail -n 1 | jq '.result' | jq -r '.[] | .name')
+    stamp=`date +"%s"`
+    nmach=$(echo $machines | wc -w)
+    if [[ $nmach -ne 3 ]]; then
+      for machine in $machines
+      do
+          whydied $machine $stamp
+      done
+    else
+      for machine in $machines
+      do
+          whylives $machine $stamp
+      done
+    fi
+    ./safescale-cover cluster delete clu-$TENANT-cent7-$fla-r$i -y
+    if [[ $RUN -eq 0 ]]; then
+      break
+    fi
+  done
+done
+
+exit $CODE
