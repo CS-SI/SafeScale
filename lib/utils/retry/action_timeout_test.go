09761bccfc1dd0e63fad4d36422dcf2bddb18c88
Oscar <oscar.picas@gmail.com>
2020-03-09 17:48:44 +0100

    Add UT to handle timeouts

---
+++ lib/utils/retry/action_timeout_test.go
@@ -0,0 +1,301 @@
+package retry
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+func randomErrGen() (string, int, error) {
+	ladder := rand.Intn(9)
+	fmt.Println(ladder)
+	switch ladder {
+	case 0:
+		return "working", 302, nil
+	case 1:
+		return "", 404, fmt.Errorf("error %d", 404)
+	case 2:
+		return "", 500, fmt.Errorf("error %d", 500)
+	case 3:
+		return "", 202, fmt.Errorf("error %d", 202)
+	case 4:
+		return "", 666, nil
+	case 5:
+		fmt.Println("Taking a nap")
+		time.Sleep(30 * time.Millisecond)
+		return "working late", 302, nil
+	case 6:
+		fmt.Println("Taking a nap with failure")
+		time.Sleep(30 * time.Millisecond)
+		return "", 667, fmt.Errorf("error %d", 667)
+	default:
+		return "somehow working", 300 + rand.Intn(5), nil
+	}
+}
+
+func TestStraight(t *testing.T) {
+	retryErr := WhileUnsuccessful(
+		func() error {
+			time.Sleep(30 * time.Millisecond)
+			return nil
+		},
+		5*time.Millisecond,
+		20*time.Millisecond,
+	)
+
+	detectedTimeout := false
+
+	if retryErr != nil {
+		if _, ok := retryErr.(ErrTimeout); ok {
+			detectedTimeout = true
+		}
+	}
+
+	if !detectedTimeout {
+		t.FailNow()
+	}
+}
+
+// WaitGroup with timeout, returns true when it's a timeout
+func waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+
+	select {
+	case <-c: // OK
+		return false
+	case <-time.After(timeout): // timeout
+		return true
+	}
+}
+
+func TestDeath(t *testing.T) {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		retryErr := WhileUnsuccessful(
+			func() error {
+				time.Sleep(30 * time.Hour)
+				return nil
+			},
+			5*time.Millisecond,
+			20*time.Millisecond,
+		)
+
+		detectedTimeout := false
+
+		if retryErr != nil {
+			if _, ok := retryErr.(ErrTimeout); ok {
+				detectedTimeout = true
+			}
+		}
+
+		if !detectedTimeout {
+			t.FailNow()
+		}
+	}()
+
+	failure := waitTimeout(&wg, time.Millisecond*200)
+	if failure {
+		t.FailNow()
+	}
+
+}
+
+func TestSurviveDeath(t *testing.T) {
+	retryErr := WhileUnsuccessfulTimeout(
+		func() error {
+			time.Sleep(30 * time.Hour)
+			return nil
+		},
+		5*time.Millisecond,
+		20*time.Millisecond,
+	)
+
+	detectedTimeout := false
+
+	if retryErr != nil {
+		if _, ok := retryErr.(ErrTimeout); ok {
+			detectedTimeout = true
+		}
+	}
+
+	if !detectedTimeout {
+		t.FailNow()
+	}
+}
+
+func TestHitTimeoutBasic(t *testing.T) {
+	rand.Seed(86)
+
+	hitTimeout := false
+	notfound := false
+
+	for i := 0; i < 2000; i++ {
+		fmt.Println("--> Begin")
+		var eserver string
+
+		retryErr := WhileUnsuccessful(
+			func() error {
+				server, code, ierr := randomErrGen()
+				if ierr != nil {
+					switch code {
+					case 404:
+						// If error is "resource not found", we want to return GopherCloud error as-is to be able
+						// to behave differently in this special case. To do so, stop the retry
+						notfound = true
+						return nil
+					case 500:
+						// When the response is "Internal Server Error", retries
+						log.Debugf("received 'Internal Server Error', retrying...")
+						return ierr
+					case 667:
+						return ierr
+					default:
+						// Any other error stops the retry
+						log.Debugf("unexpected error")
+						return nil
+					}
+				}
+
+				if server == "" {
+					return fmt.Errorf("error getting host, nil response from gophercloud")
+				}
+				eserver = server
+
+				if code >= 300 {
+					lastState := code - 300
+
+					if lastState != 4 && lastState != 2 {
+						return nil
+					}
+				}
+
+				return fmt.Errorf("server not ready yet")
+			},
+			5*time.Millisecond,
+			20*time.Millisecond,
+		)
+		if retryErr != nil {
+			if _, ok := retryErr.(ErrTimeout); ok {
+				fmt.Println("It IS a timeout !!")
+				hitTimeout = true
+				goto endgame
+			}
+			fmt.Printf("It's NOT a timeout !!: %s\n", retryErr.Error())
+			goto endgame
+		}
+		if eserver == "" || notfound {
+			fmt.Println("Not found !!")
+			goto endgame
+		}
+
+		fmt.Println("Doing something else...")
+
+	endgame:
+		fmt.Println("This is it")
+	}
+
+	if !hitTimeout {
+		t.FailNow()
+	}
+}
+
+func alwaysFailsErrGen() (string, int, error) {
+	ladder := rand.Intn(9)
+	switch ladder {
+	case 1:
+		return "", 404, fmt.Errorf("error %d", 404)
+	case 2:
+		return "", 500, fmt.Errorf("error %d", 500)
+	default:
+		return "", 667, fmt.Errorf("error %d", 667)
+	}
+}
+
+func TestHitTimeout(t *testing.T) {
+	rand.Seed(86)
+
+	hitTimeout := false
+	notfound := false
+
+	for i := 0; i < 2000; i++ {
+		fmt.Println("--> Begin")
+		var eserver string
+
+		notfound = false
+
+		retryErr := WhileUnsuccessful(
+			func() error {
+				server, code, ierr := alwaysFailsErrGen()
+				if ierr != nil {
+					switch code {
+					case 404:
+						// If error is "resource not found", we want to return GopherCloud error as-is to be able
+						// to behave differently in this special case. To do so, stop the retry
+						notfound = true
+						return nil
+					case 500:
+						// When the response is "Internal Server Error", retries
+						log.Debugf("received 'Internal Server Error', retrying...")
+						return ierr
+					case 667:
+						return ierr
+					default:
+						// Any other error stops the retry
+						log.Debug("unknown error")
+						return nil
+					}
+				}
+
+				if server == "" {
+					return fmt.Errorf("error getting host, nil response from gophercloud")
+				}
+				eserver = server
+
+				if code >= 300 {
+					lastState := code - 300
+
+					if lastState != 4 && lastState != 2 {
+						return nil
+					}
+				}
+
+				return fmt.Errorf("server not ready yet")
+			},
+			5*time.Millisecond,
+			20*time.Millisecond,
+		)
+		if retryErr != nil {
+			if _, ok := retryErr.(ErrTimeout); ok {
+				fmt.Println("It's a timeout !!")
+				hitTimeout = true
+				goto endgame
+			}
+			fmt.Printf("It's NOT a timeout !!: %s\n", retryErr.Error())
+			goto endgame
+		}
+		if eserver == "" || notfound {
+			fmt.Println("Not found !!")
+			goto endgame
+		}
+
+		fmt.Println("Doing something else...")
+
+	endgame:
+		fmt.Println("This is it")
+	}
+
+	if !hitTimeout {
+		t.FailNow()
+	}
+}
