021e1127eefc0a14e44b8b3a9fe4cce90aab6756
Oscar <oscar.picas@gmail.com>
2020-03-10 16:14:23 +0100

    Fixed docker and CI problems detected after merging branch firewalld-issue into release/v20.01

--- build/build-safescale.sh
+++ build/build-safescale.sh
@@ -25,7 +25,7 @@ echo "Cloning branch '${BRANCH_NAME}' from repo '${GIT_REPO_URL}'"
 git clone ${GIT_REPO_URL} -b ${BRANCH_NAME} --depth=1
 
 cd SafeScale
-sed -i "s/\(.*\)develop/\1${BRANCH_NAME}/" common.mk
+sed -i "s#\(.*\)develop#\1${BRANCH_NAME}#" common.mk
 
 # ----------------------
 # Compile
