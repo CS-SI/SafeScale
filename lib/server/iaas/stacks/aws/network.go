e86ef12a8ac28869a0431d278625c3e52bc32ee2
Vincent Planchenault <vincent.planchenault@c-s.fr>
2020-03-25 14:45:22 +0100

    Changes in KeyPair generations
    - implemented GenerateRSAKeyPair() in package lib/utils/crypt to mutualize ssh key pair creation code
    - updated keypair creation where needed to use GenerateRSAKeyPair
    + code cleanup
    + missing copyright header in some files

--- lib/server/iaas/stacks/aws/network.go
+++ lib/server/iaas/stacks/aws/network.go
@@ -1,7 +1,26 @@
+/*
+ * Copyright 2018-2020, CS Systemes d'Information, http://www.c-s.fr
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package aws
 
 import (
 	"fmt"
+	"net"
+	"reflect"
+
 	"github.com/CS-SI/SafeScale/lib/server/iaas/resources"
 	"github.com/CS-SI/SafeScale/lib/server/iaas/resources/enums/hostproperty"
 	"github.com/CS-SI/SafeScale/lib/server/iaas/resources/enums/hoststate"
@@ -16,8 +35,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/sirupsen/logrus"
-	"net"
-	"reflect"
 
 	propsv1 "github.com/CS-SI/SafeScale/lib/server/iaas/resources/properties/v1"
 )
