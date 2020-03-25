66d63cd9b13c942994b6317f3dc6a7d929ef19ab
oscar <oscar.picas@gmail.com>
2020-03-25 10:42:34 +0100

    Started handling 40x error codes

--- lib/server/iaas/stacks/openstack/support.go
+++ lib/server/iaas/stacks/openstack/support.go
@@ -9,6 +9,10 @@ func GetUnexpectedGophercloudErrorCode(err error) (int64, error) {
 	xType := reflect.TypeOf(err)
 	xValue := reflect.ValueOf(err)
 
+	if xValue.Kind() != reflect.Struct {
+		return 0, fmt.Errorf("not a gophercloud.ErrUnexpectedResponseCode")
+	}
+
 	_, there := xType.FieldByName("ErrUnexpectedResponseCode")
 	if there {
 		_, there := xType.FieldByName("Actual")
