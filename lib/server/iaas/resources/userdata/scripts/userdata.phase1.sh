da69880ef859a38117abbcb0a98e6825d04f2e6b
Vincent Planchenault <vincent.planchenault@c-s.fr>
2020-04-06 16:39:09 +0200

    Invalid owner:group set on /opt/safescale when OperatorUsername is set
    - this prevents the user to ssh correctly

--- lib/server/iaas/resources/userdata/scripts/userdata.phase1.sh
+++ lib/server/iaas/resources/userdata/scripts/userdata.phase1.sh
@@ -109,7 +109,7 @@ pathprepend $HOME/.local/bin
 pathappend /opt/safescale/bin
 EOF
 
-	chown -R safescale:safescale /opt/safescale
+	chown -R {{.User}}:{{.User}} /opt/safescale
 	chmod -R 0640 /opt/safescale
 	find /opt/safescale -type d -exec chmod a+rx {} \;
 	chmod 1777 /opt/safescale/var/tmp
