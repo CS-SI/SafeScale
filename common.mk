31ef70c1ded989cd86014652afd4fc5e8404461b
oscar <oscar.picas@gmail.com>
2020-04-10 16:13:36 +0200

    Fix windows build

--- common.mk
+++ common.mk
@@ -62,6 +62,10 @@ RM = del /Q
 endif
 endif
 
+ifeq ($(OS),Windows_NT)
+MAKE=mingw32-make.exe
+endif
+
 GOPATH?=$(HOME)/go
 GOBIN?=$(GOPATH)/bin
 CIBIN?=/tmp
@@ -80,14 +84,29 @@ GOBIN=$(HOME)/go/bin
 endif
 
 # Handling multiple gopath: use $(HOME)/go by default
+ifneq ($(OS),Windows_NT)
 ifeq ($(findstring :,$(GOPATH)),:)
-GOINCLUDEPATH=$(HOME)/go
+ifeq (, $(GOMODPATH))
+$(error "Having a GOPATH with several directories is not recommended, when you have such a GOPATH: [$(GOPATH)], you must specify where your go modules are installed; by default the build looks for modules in 'GOMODPATH/pkg/mod' directory, so you must export the GOMODPATH variable before running the build")
+endif
 else
-GOINCLUDEPATH=$(GOPATH)
+GOMODPATH?=$(GOPATH)
+endif
 endif
 
+ifeq ($(OS),Windows_NT)
+ifeq ($(findstring ;,$(GOPATH)),;)
+ifeq (, $(GOMODPATH))
+$(error "Having a GOPATH with several directories is not recommended, when you have such a GOPATH: [$(GOPATH)], you must specify where your go modules are installed; by default the build looks for modules in 'GOMODPATH/pkg/mod' directory, so you must export the GOMODPATH variable before running the build")
+endif
+else
+GOMODPATH?=$(GOPATH)
+endif
+endif
+
+
 ifeq ($(strip $(GOPATH)),)
-GOINCLUDEPATH=$(HOME)/go
+GOMODPATH?=$(HOME)/go
 endif
 
 ifneq ($(OS),Windows_NT)
