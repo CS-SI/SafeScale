5ba91e26114f257b967e13110a02fc06f169aeb5
oscar <oscar.picas@gmail.com>
2020-05-25 10:38:47 +0200

    Changes in integration tests Fixed errors in features

--- integrationtests/aws/test-documented-features/validation_documented_features_test.go
+++ integrationtests/aws/test-documented-features/validation_documented_features_test.go
@@ -15,10 +15,6 @@ func Test_DockerNotGateway(t *testing.T) {
 	integrationtests.DockerNotGateway(t, providers.AWS)
 }
 
-func Test_DockerCompose(t *testing.T) {
-	integrationtests.DockerCompose(t, providers.AWS)
-}
-
 func Test_RemoteDesktop(t *testing.T) {
 	integrationtests.RemoteDesktop(t, providers.AWS)
 }
