739ea76570e83a329471a9d054bca66108ed6cd0
oscar <oscar.picas@gmail.com>
2020-06-12 15:38:35 +0200

    Updated user scripts - changes in supported versions

--- lib/server/iaas/resources/userdata/scripts/userdata.debug.phase1.sh
+++ lib/server/iaas/resources/userdata/scripts/userdata.debug.phase1.sh
@@ -211,7 +211,7 @@ function fail_fast_unsupported_distros() {
 			} || true
 			;;
 	  ubuntu)
-	    if [[ $(lsb_release -rs) -lt 16 ]]; then
+	    if [[ $(lsb_release -rs | cut -d. -f1) -lt 16 ]]; then
 	      echo "PROVISIONING_ERROR: Unsupported Linux distribution '$LINUX_KIND $(lsb_release -rs)'!"
 			  fail 199
 			fi
