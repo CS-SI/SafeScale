9aff99b270c0866e1822da57d2743fcd8509ddd1
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2020-07-08 16:56:33 +0200

    Changes in lib/server/iaas/stacks/outscale
    - moved normalizeError is its own file: error.go
    + code cleanup

--- lib/server/iaas/stacks/outscale/error.go
+++ lib/server/iaas/stacks/outscale/error.go
@@ -1,1 +1,55 @@
+/*
+ * Copyright 2018-2020, CS Systemes d'Information, http://www.csgroup.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package outscale
+
+import (
+	"fmt"
+	"github.com/CS-SI/SafeScale/lib/utils/scerr"
+	"github.com/outscale-dev/osc-sdk-go/osc"
+	"reflect"
+)
+
+func normalizeError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	switch realErr := err.(type) {
+	case osc.GenericOpenAPIError:
+		switch model := realErr.Model().(type) {
+		case osc.ErrorResponse:
+			switch model.Errors[0].Code {
+			case "5057":
+				return scerr.NotFoundError("network not found")
+			//case "5071": ?
+			case "9011":
+				return scerr.DuplicateError("a keypair with this name already exists")
+			case "9044":
+				return scerr.InvalidRequestError("not included in VPC CIDR")
+			default:
+				merr := model.Errors[0]
+				reqId := model.ResponseContext.RequestId
+				return  scerr.UnknownError(fmt.Sprintf("from outscale driver, code='%s', type='%s', details='%s', requestId='%s'", merr.Code, merr.Type, merr.Details, reqId))
+			}
+		default:
+			return scerr.UnknownError(fmt.Sprintf("from outscale driver, model='%s', error='%s'", reflect.TypeOf(realErr.Model()), realErr.Error()))
+		}
+	default:
+		return err
+	}
+}
