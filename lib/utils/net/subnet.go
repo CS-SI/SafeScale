32d86b328508a99abd5a89c14978461d1821f147
Vincent PLANCHENAULT <vincent.planchenault@c-s.fr>
2020-07-17 15:45:58 +0200

    Changes in package lib/utils/net
    - split up functions to do CIDR calculation from ip.go inside its own file: cidr.go
    - split up functions to do subnet calculation from ip.go inside its own file: subnet.go

---
+++ lib/utils/net/subnet.go
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018-2020, CS Systemes d'Information, http://www.c-s.fr
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net
+
+import (
+	"github.com/CS-SI/SafeScale/lib/utils/fail"
+	"net"
+)
+
+// FirstIncludedSubnet takes a parent CIDR range and gives the first subnet within it
+// with the given number of additional prefix bits 'maskAddition'.
+//
+// For example, 192.168.0.0/16, extended by 8 bits becomes 192.168.0.0/24.
+func FirstIncludedSubnet(base net.IPNet, maskAddition uint8) (net.IPNet, fail.Error) {
+	return NthIncludedSubnet(base, maskAddition, 0)
+}
+
+// NthIncludedSubnet takes a parent CIDR range and gives the 'nth' subnet within it with the
+// given numver of additional prefix bits 'maskAddition'
+//
+// For example, 192.168.0.0/16, extended by 8 bits gives as 4th subnet 192.168.4.0/24.
+func NthIncludedSubnet(base net.IPNet, maskAddition uint8, nth uint) (net.IPNet, fail.Error) {
+	ip := base.IP
+	mask := base.Mask
+
+	parentLen, addrLen := mask.Size()
+	newPrefixLen := parentLen + int(maskAddition)
+
+	if newPrefixLen > addrLen {
+		return net.IPNet{}, fail.OverflowError(nil, uint(addrLen), "insufficient address space to extend prefix of %d by %d", parentLen, maskAddition)
+	}
+
+	maxNetNum := uint64(1<<uint64(maskAddition)) - 1
+	if uint64(1) > maxNetNum {
+		return net.IPNet{}, fail.OverflowError(nil, uint(maxNetNum), "prefix extension of %d does not accommodate a subnet", maskAddition)
+	}
+
+	ipAsNumber := IPv4ToUInt32(ip)
+	bitShift := uint32(32 - newPrefixLen)
+	ipAsNumber |= uint32(nth) << bitShift
+	return net.IPNet{
+		IP:   UInt32ToIPv4(ipAsNumber),
+		Mask: net.CIDRMask(newPrefixLen, addrLen),
+	}, nil
+}
