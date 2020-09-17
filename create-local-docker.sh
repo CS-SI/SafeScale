7b9bf32fa29fe722d3dc0b509a3fe8afb4c5e039
oscar <oscar.picas@gmail.com>
2020-09-17 11:06:27 +0200

    build docker image with go 1.15.2

--- create-local-docker.sh
+++ create-local-docker.sh
@@ -23,7 +23,7 @@ fi
 
 stamp=`date +"%s"`
 
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD) PROTOVERSION=3.6.1 GOVERSION=1.13.5 envsubst <Dockerfile.local > Dockerfile.$stamp
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD) PROTOVERSION=3.6.1 GOVERSION=1.15.2 envsubst <Dockerfile.local > Dockerfile.$stamp
 docker build --rm --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f ${WRKDIR}/Dockerfile.$stamp -t safescale:local-$(git rev-parse --abbrev-ref HEAD | sed 's#/#\-#g') ${WRKDIR}
 if [ $? -ne 0 ]; then
   echo "Docker build failed !!"
