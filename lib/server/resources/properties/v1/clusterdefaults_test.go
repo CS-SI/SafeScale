03391d4deab76d697db2c9672b657241047545d6
Vincent Planchenault <vincent.planchenault@ctacat.net>
2021-02-10 19:46:52 +0100

    Fixes to please 'make style'

--- lib/server/resources/properties/v1/clusterdefaults_test.go
+++ lib/server/resources/properties/v1/clusterdefaults_test.go
@@ -1,34 +1,34 @@
 package propertiesv1
 
 import (
-    "reflect"
-    "testing"
+	"reflect"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 
-    "github.com/CS-SI/SafeScale/lib/server/resources/abstract"
+	"github.com/CS-SI/SafeScale/lib/server/resources/abstract"
 )
 
 func TestDefaults_Clone(t *testing.T) {
-    ct := newClusterDefaults()
-    ct.Image = "something"
-    ct.GatewaySizing = abstract.HostEffectiveSizing{
-        RAMSize: 3,
-        GPUType: "NVidia",
-    }
+	ct := newClusterDefaults()
+	ct.Image = "something"
+	ct.GatewaySizing = abstract.HostEffectiveSizing{
+		RAMSize: 3,
+		GPUType: "NVidia",
+	}
 
-    clonedCt, ok := ct.Clone().(*ClusterDefaults)
-    if !ok {
-        t.Fail()
-    }
+	clonedCt, ok := ct.Clone().(*ClusterDefaults)
+	if !ok {
+		t.Fail()
+	}
 
-    assert.Equal(t, ct, clonedCt)
-    clonedCt.GatewaySizing.GPUNumber = 7
-    clonedCt.GatewaySizing.GPUType = "Culture"
+	assert.Equal(t, ct, clonedCt)
+	clonedCt.GatewaySizing.GPUNumber = 7
+	clonedCt.GatewaySizing.GPUType = "Culture"
 
-    areEqual := reflect.DeepEqual(ct, clonedCt)
-    if areEqual {
-        t.Error("It's a shallow clone !")
-        t.Fail()
-    }
+	areEqual := reflect.DeepEqual(ct, clonedCt)
+	if areEqual {
+		t.Error("It's a shallow clone !")
+		t.Fail()
+	}
 }
--- lib/utils/consts.go
+++ lib/utils/consts.go
@@ -1,16 +1,10 @@
 package utils
 
 const (
-    // BaseFolder is the path of the base folder containing safescale data
-    BaseFolder = "/opt/safescale"
-    // BinFolder is the path of the folder containing safescale binaries
-    BinFolder = BaseFolder + "/bin"
-    // VarFolder ...
-    VarFolder = BaseFolder + "/var"
-    // LogFolder ...
-    LogFolder = VarFolder + "/log"
-    // TempFolder ...
-    TempFolder = VarFolder + "/tmp"
-    // StateFolder ...
-    StateFolder = VarFolder + "/state"
+	BaseFolder  = "/opt/safescale"     // is the path of the base folder containing safescale data on cloud provider instances
+	BinFolder   = BaseFolder + "/bin"  // is the path of the folder containing safescale binaries on cloud provider instances
+	VarFolder   = BaseFolder + "/var"  // is the path of the folder containing safescale equivalent of /var
+	LogFolder   = VarFolder + "/log"   // is the path of the folder containing safescale logs
+	TempFolder  = VarFolder + "/tmp"   // is the path of the folder containing safescale temporary files
+	StateFolder = VarFolder + "/state" // is the path of the folder containing safescale states
 )
