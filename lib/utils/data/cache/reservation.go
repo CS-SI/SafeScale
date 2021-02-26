aeba57e6b9f50942e4441cc34b3130b73fb9cb43
Vincent Planchenault <vincent.planchenault@csgroup.eu>
2021-02-26 18:19:37 +0100

    Small changes to make make style happy

--- lib/utils/data/cache/reservation.go
+++ lib/utils/data/cache/reservation.go
@@ -49,9 +49,7 @@ func (rc reservation) RemoveObserver(task concurrency.Task, name string) error {
 }
 
 func (rc reservation) Released(task concurrency.Task) {
-	return //nolint
 }
 
 func (rc reservation) Destroyed(task concurrency.Task) {
-	return //nolint
 }
