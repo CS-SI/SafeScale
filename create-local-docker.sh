b311113ae7028742c1bd39f2bc2e4aeb0126fb8c
oscar <oscar.picas@gmail.com>
2021-03-15 17:16:11 +0100

    Fixed docker build

--- create-local-docker.sh
+++ create-local-docker.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 WRKDIR=$(readlink -f $(dirname "$0"))
 
+VERNAME=$(git rev-parse --abbrev-ref HEAD | awk -F/ '{print $NF}')
+
 if [ ! -f ./marker ]; then
-  curl https://api.github.com/repos/CS-SI/SafeScale/commits/$(git rev-parse --abbrev-ref HEAD) 2>&1 | grep '"date"' | tail -n 1 > ./marker
+  curl https://api.github.com/repos/CS-SI/SafeScale/commits/$VERNAME 2>&1 | grep '"date"' | tail -n 1 > ./marker
 else
-  curl https://api.github.com/repos/CS-SI/SafeScale/commits/$(git rev-parse --abbrev-ref HEAD) 2>&1 | grep '"date"' | tail -n 1 > ./newMarker
+  curl https://api.github.com/repos/CS-SI/SafeScale/commits/$VERNAME 2>&1 | grep '"date"' | tail -n 1 > ./newMarker
   diff ./marker ./newMarker && rm ./newMarker || mv ./newMarker ./marker
 fi
 
-docker build --rm --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f ${WRKDIR}/Dockerfile.local -t safescale:local-$(git rev-parse --abbrev-ref HEAD) ${WRKDIR}
+docker build --rm --network host --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f ${WRKDIR}/Dockerfile.local -t safescale:local-$VERNAME ${WRKDIR}
 [ $? -ne 0 ] && echo "Docker build failed !!" && rm -f ./marker && return 1
 
 echo "Docker build OK"
 
-docker create -ti --name dummy safescale:local-$(git rev-parse --abbrev-ref HEAD) bash
+docker create -ti --name dummy safescale:local-$VERNAME bash
 [ $? -ne 0 ] && echo "Failure extracting binaries 1/3" && return 1
 docker cp dummy:/exported .
 [ $? -ne 0 ] && echo "Failure extracting binaries 2/3" && return 1
