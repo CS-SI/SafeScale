b007addc472201f28b7474d286a26a45b31d50c1
oscar <oscar.picas@gmail.com>
2021-03-15 22:40:19 +0100

    Changes building docker images coming from the v20.06 branch

--- build-safescale-local.sh
+++ build-safescale-local.sh
@@ -1,4 +1,9 @@
-#! /bin/sh
+#! /bin/bash
+
+echo "Checks..."
+if [[ ! -v BUILD_ENV ]]; then
+    echo "BUILD_ENV is not set, this script is intended to run inside a docker container" && exit 1
+fi
 
 # ----------------------
 # Create working directory
@@ -9,46 +14,49 @@ WRKDIR=${GOPATH=/go}
 cd ${WRKDIR}
 
 cd SafeScale
-sed -i "s/\(.*\)develop/\1${BRANCH_NAME}/" common.mk
+sed -i "s#\(.*\)develop#\1${BRANCH_NAME}#" common.mk
 
 # ----------------------
 # Compile
 # ----------------------
 echo "Get dev deps"
 make getdevdeps
-[ $? -ne 0 ] && echo "Build getdevdeps failure" && return 1
+[ $? -ne 0 ] && echo "Build getdevdeps failure" && exit 1
 
-sleep 2
-
-echo "Get dev deps"
-make getdevdeps
-[ $? -ne 0 ] && echo "Build getdevdeps failure" && return 1
-
-sleep 2
+sleep 45
+hash -r
 
 echo "Get dev deps"
 make getdevdeps
-[ $? -ne 0 ] && echo "Build getdevdeps failure" && return 1
+[ $? -ne 0 ] && echo "Build getdevdeps failure" && exit 1
 
-sleep 2
+sleep 45
+hash -r
+
+echo "Get dev deps"
+make getdevdeps
+[ $? -ne 0 ] && echo "Build getdevdeps failure" && exit 1
+
+sleep 45
+hash -r
 
 echo "Ensure"
 make ensure
-[ $? -ne 0 ] && echo "Build ensure failure" && return 1
+[ $? -ne 0 ] && echo "Build ensure failure" && exit 1
 
 echo "All"
 make all
-[ $? -ne 0 ] && echo "Build failure" && return 1
+[ $? -ne 0 ] && echo "Build failure" && exit 1
 
 echo "Install"
 make install
-[ $? -ne 0 ] && echo "Install failure" && return 1
+[ $? -ne 0 ] && echo "Install failure" && exit 1
 
 echo "Export"
 export CIBIN=/exported
 mkdir -p /exported
 
 CIBIN=/exported make installci
-[ $? -ne 0 ] && echo "Export failure" && return 1
+[ $? -ne 0 ] && echo "Export failure" && exit 1
 
-return 0
+exit 0
