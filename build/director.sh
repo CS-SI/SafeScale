65055edbc455bb529501f12d44470904cacdbda8
oscar <oscar.picas@gmail.com>
2021-03-16 09:41:59 +0100

    started porting CI scripts

---
+++ build/director.sh
@@ -0,0 +1,50 @@
+#! /bin/bash -x
+
+echo "Checks..."
+if [[ ! -v BUILD_ENV ]]; then
+    echo "BUILD_ENV is not set, this script is intended to run inside a docker container" && exit 1
+fi
+
+if [[ ! -v TENANT ]]; then
+    echo "TENANT is not set..." && exit 1
+fi
+
+if [[ $(echo $TENANT | grep lexible) ]]; then
+  sed -i 's/10.4/192.168/' small.sh
+  sed -i 's/10.7/192.168/' small.sh
+  sed -i 's/10.$frag.$i/192.168.$nfrag/' small.sh
+
+  sed -i 's/10.4/192.168/' medium.sh
+  sed -i 's/10.7/192.168/' medium.sh
+  sed -i 's/10.$frag.$i/192.168.$nfrag/' medium.sh
+
+  sed -i 's/10.4/192.168/' large.sh
+  sed -i 's/10.7/192.168/' large.sh
+  sed -i 's/10.$frag.$i/192.168.$nfrag/' large.sh
+
+  sed -i 's/10.4/192.168/' poc.sh
+  sed -i 's/10.7/192.168/' poc.sh
+  sed -i 's/10.$frag.$i/192.168.$nfrag/' poc.sh
+fi
+
+if [[ $(echo $TENANT | grep tscale) ]]; then
+  sed -i 's/10.4/192.168/' small.sh
+  sed -i 's/10.7/192.168/' small.sh
+  sed -i 's/10.$frag.$i/192.168.$nfrag/' small.sh
+
+  sed -i 's/10.4/192.168/' medium.sh
+  sed -i 's/10.7/192.168/' medium.sh
+  sed -i 's/10.$frag.$i/192.168.$nfrag/' medium.sh
+
+  sed -i 's/10.4/192.168/' large.sh
+  sed -i 's/10.7/192.168/' large.sh
+  sed -i 's/10.$frag.$i/192.168.$nfrag/' large.sh
+
+  sed -i 's/10.4/192.168/' poc.sh
+  sed -i 's/10.7/192.168/' poc.sh
+  sed -i 's/10.$frag.$i/192.168.$nfrag/' poc.sh
+fi
+
+./poc.sh || true
+
+exit 0
