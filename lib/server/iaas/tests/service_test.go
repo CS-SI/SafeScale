c7a06071fdf5c239f3b93f47d9fdad2b86a34a79
Vincent Planchenault <vincent.planchenault@csgroup.eu>
2021-06-11 15:39:30 +0200

    Added UT for new search image algorithm
    - brute force for now (lot of copy/paste; would need to be refined)

--- lib/server/iaas/tests/service_test.go
+++ lib/server/iaas/tests/service_test.go
@@ -19,6 +19,7 @@ package tests
 import (
 	"encoding/json"
 	"io/ioutil"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -85,6 +86,71 @@ func SearchImageOriginal(svc iaas.Service, osname string) (*abstract.Image, fail
 
 	logrus.Infof("Selected image: '%s' (ID='%s')", imgs[maxi].Name, imgs[maxi].ID)
 	return &imgs[maxi], nil
+}
+
+func SearchImageNew(svc iaas.Service, osname string) (*abstract.Image, fail.Error) {
+	imgs, xerr := svc.ListImages(false)
+	if xerr != nil {
+		return nil, xerr
+	}
+
+	reg, err := regexp.Compile("[^A-Z0-9.]")
+	if err != nil {
+		return nil, fail.ConvertError(err)
+	}
+
+	var maxLength int
+	for _, img := range imgs {
+		length := len(img.Name)
+		if maxLength < length {
+			maxLength = length
+		}
+	}
+
+	normalizedOSName := normalizeString(osname, reg)
+	paddedNormalizedOSName := addPadding(normalizedOSName, maxLength)
+
+	minWFScore := -1
+	wfSelect := -1
+	for i, entry := range imgs {
+		normalizedImageName := normalizeString(entry.Name, reg)
+		normalizedImageName = addPadding(normalizedImageName, maxLength)
+		wfScore := smetrics.WagnerFischer(paddedNormalizedOSName, normalizedImageName, 1, 1, 2)
+		logrus.Tracef("%*s (%s): WagnerFischerScore:%4d", maxLength, entry.Name, normalizedImageName, wfScore)
+
+		if minWFScore == -1 || wfScore < minWFScore {
+			if strings.Contains(normalizedImageName, normalizedOSName) {
+				minWFScore = wfScore
+				wfSelect = i
+			}
+		}
+	}
+
+	if wfSelect < 0 {
+		return nil, fail.NotFoundError("unable to find an image matching '%s'", osname)
+	}
+
+	logrus.Infof("Selected image: '%s' (ID='%s')", imgs[wfSelect].Name, imgs[wfSelect].ID)
+	return &imgs[wfSelect], nil
+}
+
+func normalizeString(in string, reg *regexp.Regexp) string {
+	in = strings.ToUpper(in)
+	in = reg.ReplaceAllString(in, "")
+	return in
+}
+
+func addPadding(in string, maxLength int) string {
+	if maxLength <= 0 {
+		return in
+	}
+
+	length := len(in)
+	if length < maxLength {
+		paddingRight := maxLength - length
+		in += strings.Repeat(" ", paddingRight)
+	}
+	return in
 }
 
 func Test_service_SearchImage(t *testing.T) {
@@ -203,8 +269,8 @@ func Test_service_SearchImage_AWS_Centos7(t *testing.T) {
 
 	// Notice here how Jarowinkler doesn't get right the major OS version number
 	expected := &abstract.Image{
-		ID:   "ami-0057f9b19b88b562c",
-		Name: "CentOS 8.4.2105 x86_64",
+		ID:   "ami-083ffd9a223815e2c",
+		Name: "CentOS 7.4 - 12.2EE-56f0bdc8-e3e8-4b71-a1cb-0d37794843ac-ami-0c7021056b8e506d3.4",
 	}
 
 	// those are the true results of a request "CentOS 7.4" using AWS on west-3
@@ -286,3 +352,136 @@ func Test_service_SearchImage_FE_Centos7(t *testing.T) {
 // Capture the outputs for other providers, put that into another json file and change the parameter of getImages accordingly
 // In order to test other selection algorithms, just create another function like SearchImageOriginal with your own algo and
 // use your function instead of SearchImageOriginal
+
+func Test_service_SearchImageNew(t *testing.T) {
+	// the file aws-west3-images.json has been created with:
+	// safescale image list > aws-west3-images.json
+
+	// this turns the json output into the list of images we would receive from the true AWS service
+	// we don't actually use the network to run this test
+	recovered, err := getImages("aws-west3-images.json")
+	if err != nil {
+		t.FailNow()
+	}
+
+	// we are mocking the output of the ListImages method -> so we are using ListImagesMock
+	mc := minimock.NewController(t)
+	common := mocks.NewServiceMock(mc)
+	common.ListImagesMock.Expect(false).Return(recovered, nil)
+
+	// now the tricky part, ideally we should use the code 'SearchImage' from the service, but we cannot
+	// this is a code smell, it means that our code is really hard to test...
+	// in order to do so, we created a new function, SearchImageOriginal, which is testable, this is, we can replace
+	// true implementations by our mocks without touching anything else
+	// the contents of our SearchImageOriginal are the same as in iaas/service.go:685
+	res, err := SearchImageNew(common, "Ubuntu 18.04")
+	if err != nil {
+		t.FailNow()
+	}
+
+	// those are the true results of a request "Ubuntu 18.04" using AWS on west-3
+	// now we can play tuning parameters in SearchImageNew and see what happens
+	expected := &abstract.Image{
+		ID:   "ami-00187db91863e8905",
+		Name: "ubuntu-18.04-pke-201912101339",
+	}
+
+	if res.Name != expected.Name {
+		t.Errorf("It seems that we selected %s, (expected %s)", res.Name, expected.Name)
+		t.FailNow()
+	}
+
+	if res.ID != expected.ID {
+		t.Errorf("It seems that we had the wrong ID %s, (expected %s)", res.ID, expected.ID)
+		t.FailNow()
+	}
+}
+
+func Test_service_SearchImageNew_AWS_Ubu20(t *testing.T) {
+	// the file aws-west3-images.json has been created with:
+	// safescale image list > aws-west3-images.json
+
+	// this turns the json output into the list of images we would receive from the true AWS service
+	// we don't actually use the network to run this test
+	recovered, err := getImages("aws-west3-images.json")
+	if err != nil {
+		t.FailNow()
+	}
+
+	// we are mocking the output of the ListImages method -> so we are using ListImagesMock
+	mc := minimock.NewController(t)
+	common := mocks.NewServiceMock(mc)
+	common.ListImagesMock.Expect(false).Return(recovered, nil)
+
+	// now the tricky part, ideally we should use the code 'SearchImage' from the service, but we cannot
+	// this is a code smell, it means that our code is really hard to test...
+	// in order to do so, we created a new function, SearchImageOriginal, which is testable, this is, we can replace
+	// true implementations by our mocks without touching anything else
+	// the contents of our SearchImageOriginal are the same as in iaas/service.go:685
+	res, err := SearchImageNew(common, "Ubuntu 20.04")
+	if err != nil {
+		t.FailNow()
+	}
+
+	expected := &abstract.Image{
+		ID:   "ami-01af5da8f57705476",
+		Name: "ubuntu-20.04-pke-1.17.5-202007101153",
+	}
+
+	// those are the true results of a request "Ubuntu 20.04" using AWS on west-3
+	// now we can play tuning parameters in SearchImageOriginal and see what happens
+	if res.Name != expected.Name {
+		t.Errorf("It seems that we selected %s, (expected %s)", res.Name, expected.Name)
+		t.FailNow()
+	}
+
+	if res.ID != expected.ID {
+		t.Errorf("It seems that we had the wrong ID %s, (expected %s)", res.ID, expected.ID)
+		t.FailNow()
+	}
+}
+
+func Test_service_SearchImageNew_AWS_Centos7(t *testing.T) {
+	// the file aws-west3-images.json has been created with:
+	// safescale image list > aws-west3-images.json
+
+	// this turns the json output into the list of images we would receive from the true AWS service
+	// we don't actually use the network to run this test
+	recovered, err := getImages("aws-west3-images.json")
+	if err != nil {
+		t.FailNow()
+	}
+
+	// we are mocking the output of the ListImages method -> so we are using ListImagesMock
+	mc := minimock.NewController(t)
+	common := mocks.NewServiceMock(mc)
+	common.ListImagesMock.Expect(false).Return(recovered, nil)
+
+	// now the tricky part, ideally we should use the code 'SearchImage' from the service, but we cannot
+	// this is a code smell, it means that our code is really hard to test...
+	// in order to do so, we created a new function, SearchImageOriginal, which is testable, this is, we can replace
+	// true implementations by our mocks without touching anything else
+	// the contents of our SearchImageOriginal are the same as in iaas/service.go:685
+	res, err := SearchImageNew(common, "CentOS 7.4")
+	if err != nil {
+		t.FailNow()
+	}
+
+	expected := &abstract.Image{
+		ID:   "ami-083ffd9a223815e2c",
+		Name: "CentOS 7.4 - 12.2EE-56f0bdc8-e3e8-4b71-a1cb-0d37794843ac-ami-0c7021056b8e506d3.4",
+	}
+
+	// those are the true results of a request "CentOS 7.4" using AWS on west-3
+	// now we can play tuning parameters in SearchImageOriginal and see what happens
+	if res.Name != expected.Name {
+		t.Errorf("It seems that we selected %s, (expected %s)", res.Name, expected.Name)
+		t.FailNow()
+	}
+
+	if res.ID != expected.ID {
+		t.Errorf("It seems that we had the wrong ID %s, (expected %s)", res.ID, expected.ID)
+		t.FailNow()
+	}
+}
+
