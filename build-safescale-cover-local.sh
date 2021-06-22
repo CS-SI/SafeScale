86d4ca6ff2cbf91aa2f8dcd6fa0927ceae22332e
oscar <oscar.picas@gmail.com>
2021-06-22 12:06:39 +0200

    Fixed docker builds

---
+++ build-safescale-cover-local.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+echo "Checks..."
+if [[ ! -v BUILD_ENV ]]; then
+    echo "BUILD_ENV is not set, this script is intended to run inside a docker container" && exit 1
+fi
+
+# ----------------------
+# Create working directory
+# ----------------------
+echo "Accessing working directory"
+BRANCH_NAME=${BRANCH_NAME:="develop"}
+WRKDIR=${GOPATH=/go}
+cd ${WRKDIR}
+
+cd SafeScale
+sed -i "s#\(.*\)develop#\1${BRANCH_NAME}#" common.mk
+
+# ----------------------
+# Compile
+# ----------------------
+
+echo "mod"
+make mod
+
+echo "All"
+make allcover
+[ $? -ne 0 ] && echo "Build failure" && exit 1
+
+echo "Install"
+make install
+[ $? -ne 0 ] && echo "Install failure" && exit 1
+
+echo "Export"
+export CIBIN=/exported
+mkdir -p /exported
+
+CIBIN=/exported make installci
+[ $? -ne 0 ] && echo "Export failure" && exit 1
+
+exit 0
