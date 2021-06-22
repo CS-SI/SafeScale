998def49fa7f68ae96ca8042cd5817dc557dbbd0
oscar <oscar.picas@gmail.com>
2021-06-22 10:53:56 +0200

    Changed UT

--- lib/utils/concurrency/taskgroup_test.go
+++ lib/utils/concurrency/taskgroup_test.go
@@ -29,6 +29,40 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// This imitates some of the code found in cluster.go
+func TestRealCharge(t *testing.T) {
+	overlord, err := NewTaskGroup(nil)
+	require.NotNil(t, overlord)
+	require.Nil(t, err)
+
+	theID, err := overlord.GetID()
+	require.Nil(t, err)
+	require.NotEmpty(t, theID)
+
+	gorrs := 800
+
+	for ind := 0; ind < gorrs; ind++ {
+		_, err := overlord.Start(func(t Task, parameters TaskParameters) (TaskResult, fail.Error) {
+			time.Sleep(time.Duration(tools.RandomInt(50, 250)) * time.Millisecond)
+			return "waiting game", nil
+		}, nil)
+		if err != nil {
+			t.Errorf("Unexpected: %s", err)
+		}
+		if tools.RandomInt(50, 250) > 200 {
+			aErr := overlord.Abort()
+			if aErr != nil {
+				t.Errorf("What, Cannot abort ??")
+				t.FailNow()
+			}
+		}
+	}
+
+	res, err := overlord.Wait()
+	require.NotNil(t, err)
+	require.NotEmpty(t, res)
+}
+
 func TestChildrenWaitingGame(t *testing.T) {
 	overlord, err := NewTaskGroup(nil)
 	require.NotNil(t, overlord)
