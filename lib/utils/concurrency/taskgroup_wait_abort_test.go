4b5739b6ad7efd75037d64db367b300d5e3d97a1
oscar <oscar.picas@gmail.com>
2021-06-25 12:10:38 +0200

    Added UT

--- lib/utils/concurrency/taskgroup_wait_abort_test.go
+++ lib/utils/concurrency/taskgroup_wait_abort_test.go
@@ -17,6 +17,8 @@
 package concurrency
 
 import (
+	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -121,8 +123,146 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpWhenWeAlreadyStartedWaiting(t 
 
 		time.Sleep(2 * time.Second)
 		if reminder {
-			t.Errorf("by now we should see panics in lines above, panics that only shows in logs and the rest of de code is unaware of")
+			t.Errorf("by now we should see panics in lines above, panics that only shows in logs and the rest of the code is unaware of")
 		}
 		// Well, we have a problem Waiting, now it's clear, and as a bonus we uncovered a problem communicating panics to function callers
 	}
+}
+
+// This tests the same thing as TestAbortThingsThatActuallyTakeTimeCleaningUpWhenWeAlreadyStartedWaiting, it just
+// runs .Abort first, then Wait
+// It fails, however it's unclear if it should work..: by design, what should happen if we abort 1st before running the wait ?
+func TestAbortThingsThatActuallyTakeTimeCleaningUpAbortAndWaitLater(t *testing.T) {
+	enough := false
+	iter := 0
+	for {
+		iter++
+		if iter > 50 {
+			break
+		}
+		if enough {
+			break
+		}
+
+		t.Log("Next") // Each time we iterate we see this line, sometimes this doesn't fail at 1st iteration
+		single, xerr := NewTaskGroup()
+		require.NotNil(t, single)
+		require.Nil(t, xerr)
+
+		bailout := make(chan string, 80) // a buffered channel
+		for ind := 0; ind < 80; ind++ {  // with the same number of tasks, good
+			_, xerr = single.StartInSubtask(
+				func(t Task, parameters TaskParameters) (TaskResult, fail.Error) {
+					for { // do some work, then look for aborted, again and again
+						// some work
+						time.Sleep(time.Duration(tools.RandomInt(20, 30)) * time.Millisecond)
+						status, xerr := t.GetStatus()
+						if xerr != nil {
+							return "Big failure...", xerr
+						}
+						// looking again and again...
+						if status == ABORTED || status == TIMEOUT {
+							// Cleaning up first before leaving... ;)
+							time.Sleep(time.Duration(tools.RandomInt(100, 800)) * time.Millisecond)
+							break
+						}
+					}
+					// We are using the classic 'send on closed channel' trick to see if Wait actually waits until everyone is DONE.
+					// If it does we will never see a panic, but if, Abort doesn't mean TellYourChildrenToAbort but
+					// actually means AbortYourChildrenAndQuitNOWWithoutWaiting, then we have a problem
+					acha := parameters.(chan string)
+					acha <- "Bailing out"
+					return "who cares", nil
+				}, bailout,
+			)
+			require.Nil(t, xerr)
+		}
+
+		// after this, some tasks will already be looking for ABORT signals
+		time.Sleep(time.Duration(65) * time.Millisecond)
+
+		xerr = single.Abort()
+		require.Nil(t, xerr)
+
+		_, xerr = single.Wait()
+		if xerr != nil {
+			t.Errorf("Failed to Wait: %s", xerr.Error()) // Of course, we did !!, we induced a panic !! didn't we ?
+			if _, ok := xerr.(*fail.ErrRuntimePanic); !ok {
+				t.Errorf("Wait, What ??, only Abort ? where is the panic ??")
+			}
+			if !strings.Contains(spew.Sdump(xerr), "panic happened") {
+				t.Errorf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
+			}
+			// or maybe we were fast enough and we are quitting only because of Abort, but no problem, we have more iterations...
+		}
+		close(bailout) // If Wait actually waits, this is closed AFTER all Tasks filled the channel, so no panics
+		// If not..., well...
+
+		reminder := false
+		if len(bailout) != 80 { // this means panic
+			reminder = true
+			t.Errorf("Not everyone finished on time !!, panic is coming !!, some tasks will hit a closed channel !!")
+			// if we now do a t.FailNow() we already proved our point (if Wait actually waited, the channel
+			// size should be 80 each time), but if we dont...
+			// we will see runtime panics on our LOGS !!, but NOT in the code
+			// with a t.FailNow() we also fail, but the test output is less frightening
+			enough = true
+		}
+
+		time.Sleep(2 * time.Second)
+		if reminder {
+			t.Errorf("by now we should see panics in lines above, panics that only shows in logs and the rest of the code is unaware of")
+		}
+		// Well, we have a problem Waiting, now it's clear, and as a bonus we uncovered a problem communicating panics to function callers
+	}
+}
+
+// This could be an open question
+// This test runs some tasks that succeed by design, so what should happen
+// What's troubling about this test, it's that non-determinisic...
+// non-deterministic ?? what ??, yes, run it enough times and you will have different outcomes, seriosly
+// sometimes the wait fails, sometimes doesn't, sometimes we have an empty result map, sometimes a populated map...
+func TestAbortAlreadyFinishedSuccessfullyThingsThenWait(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	single, xerr := NewTaskGroupWithParent(nil)
+	require.NotNil(t, single)
+	require.Nil(t, xerr)
+
+	for ind := 0; ind < 10; ind++ {
+		_, err := single.Start(func(t Task, parameters TaskParameters) (TaskResult, fail.Error) {
+			time.Sleep(time.Duration(tools.RandomInt(10, 20)) * time.Millisecond)
+			return "waiting game", nil
+		}, nil)
+		if err != nil {
+			t.Errorf("Unexpected: %s", err)
+			t.FailNow()
+		}
+	}
+
+	time.Sleep(time.Duration(100) * time.Millisecond)
+	// single should have finished a loooongtime ago...
+
+	// but we abort anyway
+	xerr = single.Abort()
+	if xerr != nil {
+		t.Errorf("Failed to abort")
+		t.FailNow()
+	}
+
+	// the question here, is why we fail ?
+	// and more, from a client point of view, why this failed ?
+	// all we have is an aborted error
+	res, xerr := single.Wait()
+	if xerr != nil {
+		t.Errorf("Failed to Wait: %v", xerr)
+	}
+
+	t.Errorf("Recovered this: %v", res)
+
+	_ = w.Close()
+	_, _ = ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
 }
