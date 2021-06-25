69d942e60324d0259acec2c4abc5e57ce05a579e
oscar <oscar.picas@gmail.com>
2021-06-25 16:41:30 +0200

    Updated UT

--- lib/utils/concurrency/taskgroup_wait_abort_test.go
+++ lib/utils/concurrency/taskgroup_wait_abort_test.go
@@ -44,7 +44,7 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpWhenWeAlreadyStartedWaiting(t 
 	iter := 0
 	for {
 		iter++
-		if iter > 50 {
+		if iter > 20 {
 			break
 		}
 		if enough {
@@ -65,12 +65,6 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpWhenWeAlreadyStartedWaiting(t 
 					for { // do some work, then look for aborted, again and again
 						// some work
 						time.Sleep(time.Duration(tools.RandomInt(20, 30)) * time.Millisecond)
-						// status, xerr := t.GetStatus()
-						// if xerr != nil {
-						//	return "Big failure...", xerr
-						// }
-						// looking again and again...
-						// if status == ABORTED || status == TIMEOUT {
 						if t.Aborted() {
 							// Cleaning up first before leaving... ;)
 							time.Sleep(time.Duration(tools.RandomInt(100, 800)) * time.Millisecond)
@@ -103,18 +97,22 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpWhenWeAlreadyStartedWaiting(t 
 
 		_, xerr = single.Wait() // 100 ms after this, .Abort() should hit
 		if xerr != nil {
-			t.Errorf("Failed to Wait: %s", xerr.Error()) // Of course, we did !!, we induced a panic !! didn't we ?
+			t.Logf("Failed to Wait: %s", xerr.Error()) // Of course, we did !!, we induced a panic !! didn't we ?
 			switch xerr.(type) {
 			case *fail.ErrAborted:
 				cause := xerr.Cause()
-				// if _, ok := cause.(*fail.ErrRuntimePanic); !ok {
-				// 	t.Errorf("Wait, What ??, only Abort ? where is the panic ??")
-				// }
 				if !strings.Contains(spew.Sdump(cause), "panic happened") {
-					t.Errorf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
+					t.Logf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
 				}
 			// or maybe we were fast enough and we are quitting only because of Abort, but no problem, we have more iterations...
+			case *fail.ErrRuntimePanic:
+				t.Logf("We catched a panic...")
+			case *fail.ErrorList:
+				if !strings.Contains(spew.Sdump(xerr), "panic happened") {
+					t.Logf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
+				}
 			default:
+				t.Errorf("Unexpected error: %v", xerr)
 			}
 		}
 		close(bailout) // If Wait actually waits, this is closed AFTER all Tasks filled the channel, so no panics
@@ -139,6 +137,125 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpWhenWeAlreadyStartedWaiting(t 
 	}
 }
 
+// Like the previous test, but adding panics into it
+func TestAbortThingsThatActuallyTakeTimeCleaningUpAndMayPanicWhenWeAlreadyStartedWaiting(t *testing.T) {
+	caught := false
+	enough := false
+	iter := 0
+	for {
+		iter++
+		if iter > 20 {
+			break
+		}
+		if enough {
+			break
+		}
+
+		t.Log("Next") // Each time we iterate we see this line, sometimes this doesn't fail at 1st iteration
+		single, xerr := NewTaskGroup()
+		require.NotNil(t, single)
+		require.Nil(t, xerr)
+		xerr = single.SetID(fmt.Sprintf("/parent-%d", iter))
+		require.Nil(t, xerr)
+
+		bailout := make(chan string, 80) // a buffered channel
+		for ind := 0; ind < 80; ind++ {  // with the same number of tasks, good
+			_, xerr = single.StartInSubtask(
+				func(t Task, parameters TaskParameters) (TaskResult, fail.Error) {
+					for { // do some work, then look for aborted, again and again
+						// some work
+						time.Sleep(time.Duration(tools.RandomInt(20, 30)) * time.Millisecond)
+						if t.Aborted() {
+							// Cleaning up first before leaving... ;)
+							time.Sleep(time.Duration(tools.RandomInt(100, 800)) * time.Millisecond)
+							break
+						}
+					}
+
+					// We are using the classic 'send on closed channel' trick to see if Wait actually waits until everyone is DONE.
+					// If it does we will never see a panic, but if Abort doesn't mean TellYourChildrenToAbort but
+					// actually means AbortYourChildrenAndQuitNOWWithoutWaiting, then we have a problem
+					acha := parameters.(chan string)
+					acha <- "Bailing out"
+
+					// flip a coin, true and we panic, false we don't
+					if tools.RandomInt(0, 2) == 1 {
+						fmt.Println("Panicking")
+						panic("head")
+					}
+					// tails
+
+					return "who cares", nil
+				}, bailout, InheritParentIDOption, AmendID(fmt.Sprintf("/child-%d", ind)),
+			)
+			require.Nil(t, xerr)
+		}
+
+		// after this, some tasks will already be looking for ABORT signals
+		time.Sleep(time.Duration(65) * time.Millisecond)
+
+		go func() {
+			// this will actually start after wait
+			time.Sleep(time.Duration(100) * time.Millisecond)
+
+			// let's have fun
+			xerr := single.Abort()
+			require.Nil(t, xerr)
+		}()
+
+		_, xerr = single.Wait() // 100 ms after this, .Abort() should hit
+		if xerr != nil {
+			t.Logf("Failed to Wait: %s", xerr.Error()) // Of course, we did !!, we induced a panic !! didn't we ?
+			switch xerr.(type) {
+			case *fail.ErrAborted:
+				cause := xerr.Cause()
+				if !strings.Contains(spew.Sdump(cause), "panic happened") {
+					t.Logf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
+				}
+			// or maybe we were fast enough and we are quitting only because of Abort, but no problem, we have more iterations...
+			case *fail.ErrRuntimePanic:
+				t.Logf("We catched a panic..., good")
+				caught = true
+				break
+			case *fail.ErrorList:
+				if !strings.Contains(spew.Sdump(xerr), "panic happened") {
+					t.Logf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
+				} else {
+					t.Logf("We catched a panic..., good")
+					caught = true
+					break
+				}
+			default:
+				t.Errorf("Unexpected error: %v", xerr)
+			}
+		}
+		close(bailout) // If Wait actually waits, this is closed AFTER all Tasks filled the channel, so no panics
+		// If not..., well...
+
+		reminder := false
+		if len(bailout) != 80 { // this means panic
+			reminder = true
+			t.Errorf("Not everyone finished on time !!, panic is coming !!, some tasks will hit a closed channel !!")
+			// if we now do a t.FailNow() we already proved our point (if Wait actually waited, the channel
+			// size should be 80 each time), but if we don't...
+			// we will see runtime panics on our LOGS !!, but NOT in the code
+			// with a t.FailNow() we also fail, but the test output is less frightening
+			enough = true
+		}
+
+		time.Sleep(2 * time.Second)
+		if reminder {
+			t.Errorf("by now we should see panics in lines above, panics that only shows in logs and the rest of the code is unaware of")
+		}
+		// Well, we have a problem Waiting, now it's clear, and as a bonus we uncovered a problem communicating panics to function callers
+	}
+
+	if !caught {
+		t.Errorf("We were unable to catch a panic...")
+		t.FailNow()
+	}
+}
+
 // This tests the same thing as TestAbortThingsThatActuallyTakeTimeCleaningUpWhenWeAlreadyStartedWaiting, it just
 // runs .Abort first, then Wait
 // It fails, however it's unclear if it should work..: by design, what should happen if we abort 1st before running the wait ?
@@ -147,7 +264,7 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpAbortAndWaitLater(t *testing.T
 	iter := 0
 	for {
 		iter++
-		if iter > 50 {
+		if iter > 20 {
 			break
 		}
 		if enough {
@@ -166,12 +283,6 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpAbortAndWaitLater(t *testing.T
 					for { // do some work, then look for aborted, again and again
 						// some work
 						time.Sleep(time.Duration(tools.RandomInt(20, 30)) * time.Millisecond)
-						// status, xerr := t.GetStatus()
-						// if xerr != nil {
-						// 	return "Big failure...", xerr
-						// }
-						// looking again and again...
-						// if status == ABORTED || status == TIMEOUT {
 						if t.Aborted() {
 							// Cleaning up first before leaving... ;)
 							time.Sleep(time.Duration(tools.RandomInt(100, 800)) * time.Millisecond)
@@ -197,14 +308,23 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpAbortAndWaitLater(t *testing.T
 
 		_, xerr = single.Wait()
 		if xerr != nil {
-			t.Errorf("Failed to Wait: %s", xerr.Error()) // Of course, we did !!, we induced a panic !! didn't we ?
-			// if _, ok := xerr.(*fail.ErrRuntimePanic); !ok {
-			// 	t.Errorf("Wait, What ??, only Abort ? where is the panic ??")
-			// }
-			if !strings.Contains(spew.Sdump(xerr), "panic happened") {
-				t.Errorf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
-			}
+			t.Logf("Failed to Wait: %s", xerr.Error()) // Of course, we did !!, we induced a panic !! didn't we ?
+			switch xerr.(type) {
+			case *fail.ErrAborted:
+				cause := xerr.Cause()
+				if !strings.Contains(spew.Sdump(cause), "panic happened") {
+					t.Logf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
+				}
 			// or maybe we were fast enough and we are quitting only because of Abort, but no problem, we have more iterations...
+			case *fail.ErrRuntimePanic:
+				t.Logf("We catched a panic...")
+			case *fail.ErrorList:
+				if !strings.Contains(spew.Sdump(xerr), "panic happened") {
+					t.Logf("What ?? the panic was just swallowed in the logs ??, the code making the call doesn't know ???, or we just stopped waiting even before the panic happened ??...")
+				}
+			default:
+				t.Errorf("Unexpected error: %v", xerr)
+			}
 		}
 		close(bailout) // If Wait actually waits, this is closed AFTER all Tasks filled the channel, so no panics
 		// If not..., well...
@@ -235,10 +355,11 @@ func TestAbortThingsThatActuallyTakeTimeCleaningUpAbortAndWaitLater(t *testing.T
 // sometimes the wait fails, sometimes doesn't, sometimes we have an empty result map, sometimes a populated map...
 func TestAbortAlreadyFinishedSuccessfullyThingsThenWait(t *testing.T) {
 	var previousRes map[string]TaskResult
+	var previousErr error
 	iter := 0
 	for {
 		iter++
-		if iter > 50 {
+		if iter > 20 {
 			break
 		}
 
@@ -281,6 +402,18 @@ func TestAbortAlreadyFinishedSuccessfullyThingsThenWait(t *testing.T) {
 		if xerr != nil {
 			t.Errorf("Failed to Wait: %v", xerr)
 		}
+
+		// check for error inconsistencies
+		if iter == 1 {
+			previousErr = xerr
+		} else {
+			if xerr != previousErr {
+				t.Errorf("Not consistent, before: %v, now: %v", previousErr, xerr)
+				t.FailNow()
+			}
+		}
+
+		// check for result inconsistencies
 		if iter == 1 {
 			previousRes = res
 		} else {
