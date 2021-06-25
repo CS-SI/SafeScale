17fc7440ce4bd954b4207a6d5ca22f3ec9b62be1
oscar <oscar.picas@gmail.com>
2021-06-25 21:49:03 +0200

    Updated UT

---
+++ lib/utils/concurrency/invalid_task_test.go
@@ -0,0 +1,113 @@
+package concurrency
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func generator() Task {
+	return &task{}
+}
+
+func tgGenerator() TaskGroup {
+	return &taskGroup{}
+}
+
+func TestInvalidTask(t *testing.T) {
+	got := generator()
+
+	err := got.Abort()
+	require.NotNil(t, err)
+
+	_, err = got.Abortable()
+	require.NotNil(t, err)
+
+	got.Aborted()
+
+	_ = got.DisarmAbortSignal()
+
+	_, err = got.GetID()
+	require.NotNil(t, err)
+
+	_ = got.GetSignature()
+
+	_, err = got.GetStatus()
+	require.NotNil(t, err)
+
+	_ = got.GetContext()
+	require.NotNil(t, err)
+
+	_, err = got.GetLastError()
+	require.NotNil(t, err)
+
+	_, err = got.GetResult()
+	require.NotNil(t, err)
+
+	err = got.SetID("")
+	require.NotNil(t, err)
+
+	_, err = got.Run(nil, nil)
+	require.NotNil(t, err)
+
+	_, err = got.RunInSubtask(nil, nil)
+	require.NotNil(t, err)
+
+	_, err = got.Start(nil, nil)
+	require.NotNil(t, err)
+
+	_, err = got.StartWithTimeout(nil, nil, 0)
+	require.NotNil(t, err)
+
+	_, err = got.StartInSubtask(nil, nil)
+	require.NotNil(t, err)
+}
+
+func TestInvalidTaskGroup(t *testing.T) {
+	got := tgGenerator()
+
+	err := got.Abort()
+	require.NotNil(t, err)
+
+	_, err = got.Abortable()
+	require.NotNil(t, err)
+
+	got.Aborted()
+
+	_ = got.DisarmAbortSignal()
+
+	_, err = got.GetID()
+	require.NotNil(t, err)
+
+	_ = got.GetSignature()
+
+	_, err = got.GetStatus()
+	require.NotNil(t, err)
+
+	_ = got.GetContext()
+	require.NotNil(t, err)
+
+	_, err = got.GetLastError()
+	require.NotNil(t, err)
+
+	_, err = got.GetResult()
+	require.NotNil(t, err)
+
+	err = got.SetID("")
+	require.NotNil(t, err)
+
+	_, err = got.Run(nil, nil)
+	require.NotNil(t, err)
+
+	_, err = got.RunInSubtask(nil, nil)
+	require.NotNil(t, err)
+
+	_, err = got.Start(nil, nil)
+	require.NotNil(t, err)
+
+	_, err = got.StartWithTimeout(nil, nil, 0)
+	require.NotNil(t, err)
+
+	_, err = got.StartInSubtask(nil, nil)
+	require.NotNil(t, err)
+}
--- lib/utils/concurrency/taskgroup_keep_records_test.go
+++ lib/utils/concurrency/taskgroup_keep_records_test.go
@@ -44,7 +44,7 @@ func TestKeepRecords(t *testing.T) {
 						mylimit--
 					}
 
-					// flip a coin, true and there is an error, false and it's not
+					// flip a coin, true and there is an error, false if it's not
 					if tools.RandomInt(0, 2) == 1 {
 						return "mistakes happen", fail.NewError("It was head")
 					}
@@ -55,8 +55,8 @@ func TestKeepRecords(t *testing.T) {
 			require.Nil(t, xerr)
 		}
 
-		// after this, some tasks will already be looking for ABORT signals
-		time.Sleep(time.Duration(65) * time.Millisecond)
+		// wait a little until we call wait
+		time.Sleep(time.Duration(100) * time.Millisecond)
 
 		res, xerr := overlord.Wait()
 		if xerr != nil {
@@ -70,6 +70,100 @@ func TestKeepRecords(t *testing.T) {
 			}
 		} else {
 			require.NotNil(t, res)
+			require.NotEmpty(t, res)
+		}
+	}
+}
+
+// Same as before but now with timeouts
+func TestKeepRecordsWhenTimeouts(t *testing.T) {
+	iter := 0
+
+	for {
+		iter++
+		if iter > 20 {
+			break
+		}
+
+		t.Log("Next") // Each time we iterate we see this line, sometimes this doesn't fail at 1st iteration
+		single, xerr := NewTaskGroup()
+		require.NotNil(t, single)
+		require.Nil(t, xerr)
+		xerr = single.SetID(fmt.Sprintf("/parent-%d", iter))
+		require.Nil(t, xerr)
+
+		for ind := 0; ind < 10; ind++ { // work with 10 tasks
+			_, xerr = single.StartInSubtask(
+				func(t Task, parameters TaskParameters) (TaskResult, fail.Error) {
+					mylimit := tools.RandomInt(5, 15)
+					for { // iterate only a few times: mylimit
+						// some work
+						time.Sleep(time.Duration(tools.RandomInt(20, 30)) * time.Millisecond)
+
+						if t.Aborted() || mylimit <= 0 {
+							// Cleaning up first before leaving... ;)
+							time.Sleep(time.Duration(tools.RandomInt(100, 800)) * time.Millisecond)
+							break
+						}
+
+						mylimit--
+					}
+
+					// flip a coin, true and there is an error, false if it's not
+					if tools.RandomInt(0, 2) == 1 {
+						return "mistakes happen", fail.NewError("It was head")
+					}
+
+					return "who cares", nil
+				}, nil, InheritParentIDOption, AmendID(fmt.Sprintf("/child-%d", ind)),
+			)
+			require.Nil(t, xerr)
+		}
+
+		for ind := 0; ind < 10; ind++ { // and 10 unfortunate ones that will likely timeout
+			_, xerr = single.StartWithTimeout(
+				func(t Task, parameters TaskParameters) (TaskResult, fail.Error) {
+					mylimit := tools.RandomInt(5, 15)
+					for { // iterate only a few times: mylimit
+						// some work
+						time.Sleep(time.Duration(tools.RandomInt(20, 30)) * time.Millisecond)
+
+						if t.Aborted() || mylimit <= 0 {
+							// Cleaning up first before leaving... ;)
+							time.Sleep(time.Duration(tools.RandomInt(100, 800)) * time.Millisecond)
+							break
+						}
+
+						mylimit--
+					}
+
+					// flip a coin, true and there is an error, false if it's not
+					if tools.RandomInt(0, 2) == 1 {
+						return "too late", fail.NewError("It was head")
+					}
+
+					return "who cares about timeout", nil
+				}, nil, 100*time.Millisecond, InheritParentIDOption, AmendID(fmt.Sprintf("/child-%d", ind)),
+			)
+			require.Nil(t, xerr)
+		}
+
+		// wait a little until we call wait
+		time.Sleep(time.Duration(100) * time.Millisecond)
+
+		res, xerr := single.Wait()
+		if xerr != nil {
+			switch xerr.(type) {
+			case *fail.ErrAborted:
+				t.Errorf("Seriously ?, Are we confusing that a group is aborted and that are reported errors in the results ??, nobody is aborting in this test")
+				t.FailNow()
+			default:
+				t.Errorf("Unexpected error: %v", xerr)
+				t.FailNow()
+			}
+		} else {
+			require.NotNil(t, res)
+			require.NotEmpty(t, res)
 		}
 	}
 }
