32671d3e3207822cf9791f648ce895e92104e44d
oscar <oscar.picas@gmail.com>
2021-07-08 11:23:57 +0200

    Updated UT

--- lib/utils/concurrency/taskgroup_wf_test.go
+++ lib/utils/concurrency/taskgroup_wf_test.go
@@ -17,6 +17,7 @@
 package concurrency
 
 import (
+	"context"
 	"fmt"
 	"math"
 	"math/rand"
@@ -30,7 +31,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestStartAfterDoneWF(t *testing.T) {
+func TestStartAfterDone(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		wg := sync.WaitGroup{}
 		wg.Add(1)
@@ -51,7 +52,7 @@ func TestStartAfterDoneWF(t *testing.T) {
 			_, err = overlord.Start(taskgenWithCustomFunc(20, 80, 5, 3, 0, 0, false, nil), nil)
 			require.Nil(t, err)
 
-			_, _, err = overlord.WaitFor(5 * time.Second)
+			_, err = overlord.Wait()
 			require.Nil(t, err)
 
 			ok, _ := overlord.IsSuccessful()
@@ -70,7 +71,7 @@ func TestStartAfterDoneWF(t *testing.T) {
 	}
 }
 
-func TestIntrospectionWF(t *testing.T) {
+func TestIntrospection(t *testing.T) {
 	for i := 0; i < 4; i++ {
 		overlord, err := NewTaskGroupWithParent(nil)
 		require.NotNil(t, overlord)
@@ -106,7 +107,7 @@ func TestIntrospectionWF(t *testing.T) {
 		ok, err := overlord.IsSuccessful()
 		require.NotNil(t, err)
 
-		_, res, err := overlord.WaitFor(5 * time.Second)
+		res, err := overlord.Wait()
 		require.Nil(t, err)
 		require.NotEmpty(t, res)
 
@@ -116,7 +117,7 @@ func TestIntrospectionWF(t *testing.T) {
 	}
 }
 
-func TestIntrospectionWithErrorsWF(t *testing.T) {
+func TestIntrospectionWithErrors(t *testing.T) {
 	overlord, err := NewTaskGroupWithParent(nil)
 	require.NotNil(t, overlord)
 	require.Nil(t, err)
@@ -163,7 +164,7 @@ func TestIntrospectionWithErrorsWF(t *testing.T) {
 	ok, err := overlord.IsSuccessful()
 	require.NotNil(t, err)
 
-	_, res, err := overlord.WaitFor(5 * time.Second)
+	res, err := overlord.Wait()
 	require.NotNil(t, err)
 	require.NotEmpty(t, res)
 
@@ -172,12 +173,40 @@ func TestIntrospectionWithErrorsWF(t *testing.T) {
 	require.False(t, ok)
 }
 
-func TestCallingReadyTaskGroupWF(t *testing.T) {
+func TestChildrenWaitingGameOnlyAWhile(t *testing.T) {
+	overlord, err := NewTaskGroup()
+	require.NotNil(t, overlord)
+	require.Nil(t, err)
+
+	theID, err := overlord.GetID()
+	require.Nil(t, err)
+	require.NotEmpty(t, theID)
+
+	for ind := 0; ind < 200; ind++ {
+		_, err := overlord.Start(func(t Task, parameters TaskParameters) (TaskResult, fail.Error) {
+			time.Sleep(time.Duration(randomInt(50, 250)) * time.Millisecond)
+			return "waiting game", nil
+		}, nil)
+		if err != nil {
+			t.Errorf("Unexpected: %s", err)
+			t.FailNow()
+		}
+	}
+
+	fastEnough, res, err := overlord.WaitFor(90 * time.Millisecond)
+	if fastEnough {
+		t.FailNow()
+	}
+	require.NotNil(t, err)
+	require.Empty(t, res)
+}
+
+func TestCallingReadyTaskGroup(t *testing.T) {
 	overlord, err := NewTaskGroupWithParent(nil)
 	require.NotNil(t, overlord)
 	require.Nil(t, err)
 
-	_, res, err := overlord.WaitFor(5 * time.Second)
+	res, err := overlord.Wait()
 	require.Empty(t, res)
 	require.NotNil(t, err)
 
@@ -198,7 +227,63 @@ func TestCallingReadyTaskGroupWF(t *testing.T) {
 	require.True(t, result)
 }
 
-func TestTimingOnlyOneWF(t *testing.T) {
+func TestChildrenWaitingGameEnoughTime(t *testing.T) {
+	funk := func(index int, rounds int, lower int, upper int, latency int, margin int, gcpressure int) {
+		failures := 0
+		for iter := 0; iter < rounds; iter++ {
+			overlord, xerr := NewTaskGroupWithParent(nil)
+			require.NotNil(t, overlord)
+			require.Nil(t, xerr)
+			xerr = overlord.SetID("/parent")
+			require.Nil(t, xerr)
+
+			theID, xerr := overlord.GetID()
+			require.Nil(t, xerr)
+			require.NotEmpty(t, theID)
+
+			begin := time.Now()
+			for ind := 0; ind < gcpressure; ind++ {
+				_, xerr := overlord.Start(taskgen(lower, upper, latency, 0, 0, 0, false), nil, InheritParentIDOption, AmendID(fmt.Sprintf("/child-%d", ind)))
+				if xerr != nil {
+					t.Errorf("Test %d: Unexpected: %s", index, xerr)
+					t.FailNow()
+					return
+				}
+			}
+			childrenStartDuration := time.Since(begin)
+			upbound := int(math.Ceil(float64(upper)/float64(latency)) * float64(latency))
+			timeout := time.Duration(upbound+margin) * time.Millisecond
+			// Waits that all children have started to access max safely
+			begin = time.Now()
+			fastEnough, res, xerr := overlord.WaitFor(timeout)
+			waitForRealDuration := time.Since(begin)
+			if !fastEnough {
+				if childrenStartDuration > 5*time.Millisecond { // however, it grows with gcpressure
+					t.Logf("Launching children took %v", childrenStartDuration)
+				}
+				t.Logf("WaitFor really waited %v/%v", waitForRealDuration, timeout)
+				t.Logf("Test %d, It should be enough time but it wasn't at iteration #%d", index, iter)
+				failures++
+				if failures > (rounds / 2) {
+					t.Errorf("Test %d: too many failures", index)
+					t.FailNow()
+					return
+				}
+			} else {
+				require.Nil(t, xerr)
+				require.NotEmpty(t, res)
+			}
+		}
+	}
+
+	// Look at the pressure supported by GC
+	funk(1, 20, 200, 250, 50, 50, 10)
+	funk(2, 20, 200, 250, 50, 50, 10)
+	funk(3, 20, 200, 250, 50, 50, 10)
+	funk(4, 20, 200, 250, 50, 50, 10)
+}
+
+func TestTimingOnlyOne(t *testing.T) {
 	funk := func(index int, rounds int, lower int, upper int, latency int, margin int, gcpressure int) {
 		failures := 0
 		for iter := 0; iter < rounds; iter++ {
@@ -227,7 +312,7 @@ func TestTimingOnlyOneWF(t *testing.T) {
 			timeout := time.Duration(upbound+margin) * time.Millisecond
 			// Waits that all children have started to access max safely
 			begin = time.Now()
-			_, res, xerr := overlord.WaitFor(5 * time.Second)
+			res, xerr := overlord.Wait()
 			waitRealDuration := time.Since(begin)
 			if waitRealDuration > timeout {
 				if childrenStartDuration > 5*time.Millisecond { // however, it grows with gcpressure
@@ -245,20 +330,35 @@ func TestTimingOnlyOneWF(t *testing.T) {
 
 	// the latency heavily impacts the results, it's not the gc, it's the increasing sleep overhead
 	funk(10, 1, 230, 250, 5, 10, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(11, 1, 230, 250, 10, 10, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(12, 1, 230, 250, 20, 10, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(13, 1, 230, 250, 40, 10, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(20, 1, 230, 250, 5, 20, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(21, 1, 230, 250, 10, 20, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(22, 1, 230, 250, 20, 20, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(23, 1, 230, 250, 40, 20, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(30, 1, 230, 250, 5, 30, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(31, 1, 230, 250, 10, 30, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(32, 1, 230, 250, 20, 30, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(33, 1, 230, 250, 40, 30, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(40, 1, 230, 250, 5, 40, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(41, 1, 230, 250, 10, 40, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(42, 1, 230, 250, 20, 40, 1)
+	time.Sleep(50 * time.Millisecond)
 	funk(43, 1, 230, 250, 40, 40, 1)
 	funk(50, 1, 230, 250, 5, 50, 1)
 	funk(51, 1, 230, 250, 10, 50, 1)
@@ -266,7 +366,7 @@ func TestTimingOnlyOneWF(t *testing.T) {
 	funk(53, 1, 230, 250, 40, 50, 1)
 }
 
-func TestChildrenWaitingGameEnoughTimeAfterWF(t *testing.T) {
+func TestChildrenWaitingGameEnoughTimeAfter(t *testing.T) {
 	funk := func(index int, rounds int, lower int, upper int, latency int, margin int, gcpressure int) {
 		failures := 0
 		for iter := 0; iter < rounds; iter++ {
@@ -295,7 +395,7 @@ func TestChildrenWaitingGameEnoughTimeAfterWF(t *testing.T) {
 			timeout := time.Duration(upbound+margin) * time.Millisecond
 			// Waits that all children have started to access max safely
 			begin = time.Now()
-			_, res, xerr := overlord.WaitFor(5 * time.Second)
+			res, xerr := overlord.Wait()
 			waitForRealDuration := time.Since(begin)
 			if waitForRealDuration > timeout {
 				if childrenStartDuration > 5*time.Millisecond { // however, it grows with gcpressure
@@ -317,13 +417,17 @@ func TestChildrenWaitingGameEnoughTimeAfterWF(t *testing.T) {
 	}
 
 	// Look at the pressure supported by GC
-	funk(1, 40, 50, 250, 20, 40, 20)
-	funk(2, 40, 50, 250, 20, 40, 20)
-	funk(3, 40, 50, 250, 20, 40, 20)
-	funk(4, 40, 50, 250, 20, 40, 20)
+	funk(1, 10, 50, 250, 20, 40, 20)
+
+	// time.Sleep(50 * time.Millisecond)
+	// funk(2, 10, 50, 250, 20, 40, 20)
+	// time.Sleep(50 * time.Millisecond)
+	// funk(3, 10, 50, 250, 20, 40, 20)
+	// time.Sleep(50 * time.Millisecond)
+	// funk(4, 10, 50, 250, 20, 40, 20)
 }
 
-func TestStatesWF(t *testing.T) {
+func TestStates(t *testing.T) {
 	overlord, xerr := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, xerr)
@@ -342,7 +446,7 @@ func TestStatesWF(t *testing.T) {
 	aborted := overlord.Aborted()
 	require.False(t, aborted)
 
-	_, res, xerr := overlord.WaitFor(5 * time.Second)
+	res, xerr := overlord.Wait()
 	require.NotNil(t, xerr)
 	require.NotEmpty(t, res)
 
@@ -374,7 +478,7 @@ func TestStatesWF(t *testing.T) {
 	require.NotEqual(t, st, gst) // this is unclear, why both a GetStatus and a GetGroupStatus ?
 }
 
-func TestTimeoutStateWF(t *testing.T) {
+func TestTimeoutState(t *testing.T) {
 	overlord, xerr := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, xerr)
@@ -408,7 +512,7 @@ func TestTimeoutStateWF(t *testing.T) {
 		t.Errorf("This should be a RUNNING and it's not: %s", st) // VPL: overlord in itself never timed out... expected value is RUNNING
 	} // To make TaskGroup times out, you have to use a Deadline on its parent context
 
-	_, res, xerr := overlord.WaitFor(5 * time.Second)
+	res, xerr := overlord.Wait()
 	require.NotNil(t, xerr) // VPL: all children ended on Timeout, but all terminates normally... So xerr is ErrorList
 	require.NotEmpty(t, res)
 
@@ -420,7 +524,7 @@ func TestTimeoutStateWF(t *testing.T) {
 	}
 }
 
-func TestGrTimeoutStateWF(t *testing.T) {
+func TestGrTimeoutState(t *testing.T) {
 	overlord, xerr := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, xerr)
@@ -456,7 +560,7 @@ func TestGrTimeoutStateWF(t *testing.T) {
 		t.Errorf("Everything should be a timeout")
 	}
 
-	_, res, xerr := overlord.WaitFor(5 * time.Second)
+	res, xerr := overlord.Wait()
 	require.NotNil(t, xerr)
 	require.NotEmpty(t, res)
 
@@ -471,7 +575,7 @@ func TestGrTimeoutStateWF(t *testing.T) {
 	}
 }
 
-func TestChildrenWaitingGameWF(t *testing.T) {
+func TestChildrenWaitingGame(t *testing.T) {
 	overlord, err := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, err)
@@ -490,12 +594,12 @@ func TestChildrenWaitingGameWF(t *testing.T) {
 		}
 	}
 
-	_, res, err := overlord.WaitFor(5 * time.Second)
+	res, err := overlord.Wait()
 	require.Nil(t, err)
 	require.NotEmpty(t, res)
 }
 
-func TestChildrenHaveDistinctIDsWF(t *testing.T) {
+func TestChildrenHaveDistinctIDs(t *testing.T) {
 	overlord, err := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, err)
@@ -520,7 +624,7 @@ func TestChildrenHaveDistinctIDsWF(t *testing.T) {
 		}
 	}
 
-	_, res, err := overlord.WaitGroupFor(5 * time.Second)
+	res, err := overlord.WaitGroup()
 	require.Nil(t, err)
 	require.NotEmpty(t, res)
 
@@ -540,7 +644,7 @@ func TestChildrenHaveDistinctIDsWF(t *testing.T) {
 	}
 }
 
-func TestChildrenWaitingGameWithPanicWF(t *testing.T) {
+func TestChildrenWaitingGameWithPanic(t *testing.T) {
 	overlord, err := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, err)
@@ -564,7 +668,7 @@ func TestChildrenWaitingGameWithPanicWF(t *testing.T) {
 		}
 	}
 
-	_, res, err := overlord.WaitGroupFor(5 * time.Second)
+	res, err := overlord.WaitGroup()
 	require.NotNil(t, err)
 	require.NotEmpty(t, res)
 
@@ -583,7 +687,7 @@ func TestChildrenWaitingGameWithPanicWF(t *testing.T) {
 	}
 }
 
-func TestChildrenWaitingGameWithRandomErrorWF(t *testing.T) {
+func TestChildrenWaitingGameWithRandomError(t *testing.T) {
 	overlord, err := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, err)
@@ -607,12 +711,145 @@ func TestChildrenWaitingGameWithRandomErrorWF(t *testing.T) {
 		}
 	}
 
-	_, res, err := overlord.WaitGroupFor(5 * time.Second)
+	res, err := overlord.WaitGroup()
 	require.NotNil(t, err)
 	require.NotEmpty(t, res)
 }
 
-func TestOneErrorOneOkWF(t *testing.T) {
+func TestChildrenTryWaitingGameWithRandomError(t *testing.T) {
+	overlord, err := NewTaskGroup()
+	require.NotNil(t, overlord)
+	require.Nil(t, err)
+
+	theID, err := overlord.GetID()
+	require.Nil(t, err)
+	require.NotEmpty(t, theID)
+
+	for ind := 0; ind < 200; ind++ {
+		_, err := overlord.Start(func(t Task, parameters TaskParameters) (TaskResult, fail.Error) {
+			rint := randomInt(50, 250)
+			time.Sleep(time.Duration(rint) * time.Millisecond)
+			if rint > 100 {
+				return "", fail.NewError("suck it")
+			}
+
+			return "waiting game", nil
+		}, nil)
+		if err != nil {
+			t.Errorf("Unexpected: %s", err)
+		}
+	}
+
+	begin := time.Now()
+	waited, res, err := overlord.TryWaitGroup()
+	end := time.Since(begin)
+
+	if end >= (time.Millisecond * 200) {
+		t.Errorf("It should have finished near 200 ms but it didn't !!")
+	}
+
+	require.False(t, waited)
+	require.Nil(t, err)
+	require.Nil(t, res)
+}
+
+func TestChildrenWaitingGameWithWait4EverTasks(t *testing.T) {
+	defer func() { // sometimes this test panics, breaking coverage collection..., so no more panics
+		if r := recover(); r != nil {
+			t.Errorf("Test panicked")
+			t.FailNow()
+		}
+	}()
+
+	overlord, err := NewTaskGroup()
+	require.NotNil(t, overlord)
+	require.Nil(t, err)
+
+	theID, err := overlord.GetID()
+	require.Nil(t, err)
+	require.NotEmpty(t, theID)
+
+	var tasks []Task
+
+	for ind := 0; ind < 200; ind++ {
+		rt, err := overlord.Start(func(ta Task, parameters TaskParameters) (TaskResult, fail.Error) {
+			defer func() { // sometimes this test panics, breaking coverage collection..., so no more panics
+				if r := recover(); r != nil {
+					t.Errorf("Test panicked")
+					t.FailNow()
+				}
+			}()
+			rint := randomInt(5, 25)
+			if rint > 8 {
+				rint += 1000
+			}
+			fmt.Printf("sleeping %dms...\n", rint)
+			time.Sleep(time.Duration(rint) * time.Millisecond)
+
+			return "waiting game", nil
+		}, nil)
+		if err != nil {
+			t.Errorf("Unexpected: %s", err)
+			t.Fail()
+		}
+		tasks = append(tasks, rt)
+	}
+
+	if len(tasks) == 0 {
+		t.Fatal("Unexpected error")
+	}
+
+	var res TaskResult
+
+	c := make(chan struct{})
+	go func() {
+		res, err = overlord.WaitGroup()
+		if err != nil {
+			t.Errorf("It shouldn't happen")
+			t.Fail()
+		}
+		c <- struct{}{} // done
+		close(c)
+	}()
+
+	select {
+	case <-time.After(time.Duration(300) * time.Millisecond):
+		stats, statsErr := overlord.GetGroupStatus()
+		if statsErr != nil {
+			t.Fatal(statsErr)
+		}
+
+		if len(stats[RUNNING]) == 0 {
+			t.Errorf("We should have dangling goroutines here...")
+		} else {
+			// fmt.Printf("We have %d dead goroutines", len(stats[RUNNING]))
+			require.True(t, len(stats[RUNNING]) > 0)
+		}
+
+	case <-c:
+		fmt.Printf("Good %s", res)
+		t.Errorf("It should have failed")
+	}
+
+	require.True(t, true)
+
+	time.Sleep(3 * time.Second) // let goroutines finish
+}
+
+func TestNewMethod(t *testing.T) {
+	overlord, err := NewTaskGroupWithParent(nil)
+	require.NotNil(t, overlord)
+	require.Nil(t, err)
+	other, err := overlord.New()
+	require.NotNil(t, other)
+	require.Nil(t, err)
+
+	overlord, err = NewTaskGroupWithContext(context.Background())
+	require.NotNil(t, overlord)
+	require.Nil(t, err)
+}
+
+func TestOneErrorOneOk(t *testing.T) {
 	overlord, err := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, err)
@@ -635,7 +872,7 @@ func TestOneErrorOneOkWF(t *testing.T) {
 
 			return nil, fail.NewError("Ouch")
 		}, nil)
-	_, _, err = overlord.WaitGroupFor(5 * time.Second)
+	_, err = overlord.WaitGroup()
 	if err != nil {
 		repr := err.Error()
 		if !strings.Contains(repr, "Ouch") {
@@ -644,7 +881,7 @@ func TestOneErrorOneOkWF(t *testing.T) {
 	}
 
 	// Wait a 2nd time
-	_, _, err = overlord.WaitGroupFor(5 * time.Second)
+	_, err = overlord.WaitGroup()
 	if err != nil {
 		repr := err.Error()
 		if !strings.Contains(repr, "Ouch") {
@@ -671,7 +908,7 @@ func TestOneErrorOneOkWF(t *testing.T) {
 	}
 }
 
-func TestChildrenWaitingGameWithTimeoutsWF(t *testing.T) {
+func TestChildrenWaitingGameWithTimeouts(t *testing.T) {
 	overlord, err := NewTaskGroup()
 	require.NotNil(t, overlord)
 	require.Nil(t, err)
@@ -711,7 +948,7 @@ func TestChildrenWaitingGameWithTimeoutsWF(t *testing.T) {
 	}
 }
 
-func TestChildrenWaitingGameWithTimeoutsButAbortingWF(t *testing.T) {
+func TestChildrenWaitingGameWithTimeoutsButAborting(t *testing.T) {
 	for j := 0; j < 100; j++ {
 		overlord, xerr := NewTaskGroup()
 		require.NotNil(t, overlord)
@@ -745,7 +982,7 @@ func TestChildrenWaitingGameWithTimeoutsButAbortingWF(t *testing.T) {
 		require.NotNil(t, xerr)
 		end = time.Since(begin)
 
-		if end >= (time.Millisecond * 100) { // this is twice the maximum time...
+		if end >= (time.Millisecond * 200) { // this is 4x the maximum time...
 			t.Logf("Abort() lasted %v\n", end)
 			t.Logf("Wait() lasted %v\n", end)
 			t.Errorf("It should have finished near 100 ms but it didn't!!")
@@ -754,7 +991,7 @@ func TestChildrenWaitingGameWithTimeoutsButAbortingWF(t *testing.T) {
 	}
 }
 
-func TestChildrenWaitingGameWithTimeoutsButAbortingInParallelWF(t *testing.T) {
+func TestChildrenWaitingGameWithTimeoutsButAbortingInParallel(t *testing.T) {
 	defer func() { // sometimes this test panics, breaking coverage collection..., so no more panics
 		if r := recover(); r != nil {
 			t.Errorf("Test panicked")
@@ -813,7 +1050,7 @@ func TestChildrenWaitingGameWithTimeoutsButAbortingInParallelWF(t *testing.T) {
 			}
 		}()
 
-		if _, _, xerr := overlord.WaitGroupFor(5 * time.Second); xerr != nil {
+		if _, xerr := overlord.WaitGroup(); xerr != nil {
 			switch xerr.(type) {
 			case *fail.ErrAborted:
 				// Wanted situation, continue
@@ -841,8 +1078,8 @@ func TestChildrenWaitingGameWithTimeoutsButAbortingInParallelWF(t *testing.T) {
 
 		fmt.Println("Here we are")
 
-		if end >= (time.Millisecond * 1000) {
-			t.Errorf("It should have finished near 1000 ms but it didn't, it was %v !!", end)
+		if end >= (time.Millisecond * 1200) {
+			t.Errorf("It should have finished near 1200 ms but it didn't, it was %v !!", end)
 		}
 	}()
 
@@ -859,4 +1096,22 @@ func TestChildrenWaitingGameWithTimeoutsButAbortingInParallelWF(t *testing.T) {
 	}
 
 	time.Sleep(3 * time.Second)
+}
+
+func BenchmarkTryWaitGroup(b *testing.B) {
+	overlord, xerr := NewTaskGroup()
+	require.Nil(b, xerr)
+	require.NotNil(b, overlord)
+
+	for ind := 0; ind < 1000; ind++ {
+		_, xerr = overlord.Start(func(t Task, _ TaskParameters) (TaskResult, fail.Error) {
+			time.Sleep(10 * time.Second)
+			return nil, nil
+		}, nil)
+		require.Nil(b, xerr)
+	}
+
+	for i := 0; i < b.N; i++ {
+		_, _, xerr = overlord.TryWaitGroup()
+	}
 }
