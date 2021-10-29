3a3647028580670bee57d338cc2f715bfd47bde4
Vincent Planchenault <vincent.planchenault@csgroup.eu>
2021-10-29 17:02:12 +0200

    Updated bash_library to support recent change in docker command outputs

--- lib/system/scripts/bash_library.sh
+++ lib/system/scripts/bash_library.sh
@@ -811,20 +811,35 @@ sfSubnetOfDockerNetwork() {
 }
 export -f sfSubnetOfDockerNetwork
 
-# tells if a container using a specific image (and optionnaly name) is running in standalone mode
-sfDoesDockerRunContainer() {
-    [ $# -eq 0 ] && return 1
-    local IMAGE=$1
-    shift
-    local INSTANCE=
-    [ $# -ge 1 ] && INSTANCE=$1
+# tells if a container using a specific image (and optional name) is running in standalone mode
+function sfDoesDockerRunContainer() {
+	[ $# -eq 0 ] && return 1
+	local IMAGE=$1
+	shift
+	local INSTANCE=
+	[ $# -ge 1 ] && INSTANCE=$1
+	# docker seems to have change .Names separator from underscore to dash in --format... May need to search for the
+	# 2 different names in docker container ls
+	local INSTANCE_2=$(echo $INSTANCE | tr -s "_" "-")
+	[ "$INSTANCE" = "$INSTANCE_2" ] && INSTANCE_2=$(echo $INSTANCE | tr -s "-" "_")
+	[ "$INSTANCE" = "$INSTANCE_2" ] && INSTANCE_2=
 
-    local LIST=$(docker container ls {{ "--format '{{.Image}}|{{.Names}}|{{.Status}}'" }})
-    [ -z "$LIST" ] && return 1
-    [ "$IMAGE" != "$(echo "$LIST" | cut -d'|' -f1 | grep "$IMAGE" | uniq)" ] && return 1
-    [ ! -z "$INSTANCE" -a "$INSTANCE" != "$(echo "$LIST" | cut -d'|' -f2 | grep "$INSTANCE" | uniq)" ] && return 1
-    echo $LIST | cut -d'|' -f3 | grep -i "^up" &>/dev/null || return 1
-    return 0
+	local LIST=$(docker container ls {{ "--format '{{.Image}}|{{.Names}}|{{.Status}}'" }})
+	[ -z "$LIST" ] && return 1
+	[ "$IMAGE" != "$(echo "$LIST" | cut -d'|' -f1 | grep "$IMAGE" | uniq)" ] && return 1
+	if [ ! -z "$INSTANCE" ]; then
+		local FOUND=n
+		if [ "$INSTANCE" == "$(echo "$LIST" | cut -d'|' -f2 | grep "$INSTANCE" | uniq)" ]; then
+			FOUND=yes
+		elif [ "$INSTANCE_2" != "$INSTANCE" ]; then
+			if [ "$INSTANCE_2" == "$(echo "$LIST" | cut -d'|' -f2 | grep "$INSTANCE_2" | uniq)" ]; then
+				found=y
+			fi
+		fi
+		[ "$FOUND" != "y"] && return 1
+	fi
+	echo $LIST | cut -d'|' -f3 | grep -i "^up" &>/dev/null || return 1
+	return 0
 }
 export -f sfDoesDockerRunContainer
 
