d949eeec576e6c7d75606d45c770269e5e93b865
oscar <oscar.picas@gmail.com>
2022-02-23 15:32:09 +0100

    Fixed linter warnings

--- lib/protocol/protocol.go
+++ lib/protocol/protocol.go
@@ -1,8 +1,8 @@
 package protocol
 
 import (
-	_ "google.golang.org/protobuf/reflect/protoreflect"
-	_ "google.golang.org/protobuf/runtime/protoimpl"
-	_ "google.golang.org/protobuf/types/known/emptypb"
-	_ "google.golang.org/protobuf/types/known/timestamppb"
+	_ "google.golang.org/protobuf/reflect/protoreflect"    // nolint
+	_ "google.golang.org/protobuf/runtime/protoimpl"       // nolint
+	_ "google.golang.org/protobuf/types/known/emptypb"     // nolint
+	_ "google.golang.org/protobuf/types/known/timestamppb" // nolint
 )
