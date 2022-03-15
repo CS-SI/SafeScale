2e3eae2011e3635d7b314267dadfa0ee0e40a0b5
oscar <oscar.picas@gmail.com>
2022-03-15 11:56:49 +0100

    Enable native ssh branch using 'tunnel' build tag

---
+++ lib/server/resources/operations/host_sync.go
@@ -0,0 +1,27 @@
+//go:build !tunnel
+// +build !tunnel
+
+package operations
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/CS-SI/SafeScale/v21/lib/utils/temporal"
+	"github.com/sirupsen/logrus"
+)
+
+func getCommand(file string) string {
+	command := fmt.Sprintf("sudo bash %s; exit $?", file)
+	logrus.Debugf("running '%s'", command)
+	return command
+}
+
+func getPhase2Timeout(timings temporal.Timings) time.Duration {
+	return timings.HostOperationTimeout()
+}
+
+func getPhase4Timeout(timings temporal.Timings) time.Duration {
+	waitingTime := temporal.MaxTimeout(4*time.Minute, timings.HostCreationTimeout())
+	return waitingTime
+}
