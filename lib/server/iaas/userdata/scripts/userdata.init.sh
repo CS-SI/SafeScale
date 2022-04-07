08513b18614e048a9933266f429b226c83514fc5
oscar <oscar.picas@gmail.com>
2022-04-07 16:12:50 +0200

    Cleaning init scripts

--- lib/server/iaas/userdata/scripts/userdata.init.sh
+++ lib/server/iaas/userdata/scripts/userdata.init.sh
@@ -135,33 +135,6 @@ EOF
   chmod -R 0600 /home/{{.Username}}/.ssh/*
   cat /home/{{.Username}}/.ssh/id_rsa
 
-  cat >> /home/{{.Username}}/.bashrc <<- EOF
-		pathremove() {
-      local IFS=':'
-      local NEWPATH=""
-      local DIR
-      local PATHVARIABLE=${2:-PATH}
-      for DIR in ${!PATHVARIABLE} ; do
-              if [ "$DIR" != "$1" ] ; then
-                  NEWPATH=${NEWPATH:+$NEWPATH:}$DIR
-              fi
-      done
-      export $PATHVARIABLE="$NEWPATH"
-		}
-		pathprepend() {
-      pathremove $1 $2
-      local PATHVARIABLE=${2:-PATH}
-      export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
-		}
-		pathappend() {
-      pathremove $1 $2
-      local PATHVARIABLE=${2:-PATH}
-      export $PATHVARIABLE="${!PATHVARIABLE:+${!PATHVARIABLE}:}$1"
-		}
-		pathprepend $HOME/.local/bin ''
-		pathappend /opt/safescale/bin ''
-EOF
-
   chown -R {{.Username}}:{{.Username}} /opt/safescale
   chmod -R 0640 /opt/safescale
   find /opt/safescale -type d -exec chmod a+rx {} \;
