650c9cce917f430ecf8fa3ad93fc49261c5018de
Vincent Planchenault <vincent.planchenault@csgroup.eu>
2022-06-29 14:24:52 +0200

    .

--- lib/system/nfs/client.go
+++ lib/system/nfs/client.go
@@ -55,9 +55,7 @@ func (c *Client) Install(ctx context.Context, svc iaas.Service) fail.Error {
 }
 
 // Mount defines a mount of a remote share and mount it
-func (c *Client) Mount(
-	ctx context.Context, svc iaas.Service, export string, mountPoint string, withCache bool,
-) fail.Error {
+func (c *Client) Mount(ctx context.Context, svc iaas.Service, export string, mountPoint string, withCache bool) fail.Error {
 	timings, xerr := svc.Timings()
 	if xerr != nil {
 		return xerr
