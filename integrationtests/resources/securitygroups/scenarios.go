7a92daf3f4892a80fd4661143e9140f102107445
oscar <oscar.picas@gmail.com>
2022-06-30 11:48:14 +0200

    Added scenario in security groups

--- integrationtests/resources/securitygroups/scenarios.go
+++ integrationtests/resources/securitygroups/scenarios.go
@@ -20,7 +20,15 @@
 package securitygroups
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+
+	"github.com/CS-SI/SafeScale/v22/integrationtests/helpers"
+	"github.com/stretchr/testify/require"
 )
 
 func SecurityGroupLife(t *testing.T) {
@@ -31,14 +39,90 @@ func AddRuleToExistingSecurityGroup(t *testing.T) {
 	// FIXME: implement this test
 }
 
-// CheckDefaultSecGroups checks that all ports (except 22) are closed by default
-func CheckDefaultSecGroups(t *testing.T) {
-	// FIXME: Implement this test
-}
+func OpenPortClosedByDefaultInGateway(t *testing.T) {
+	names := helpers.GetNames("BasicTest", 0, 0, 0, 0, 1, 0, 0, 0)
+	names.TearDown()
+	defer names.TearDown()
 
-// OpenPort unblocks a port in a gateway, runs a server in the gateway, then accesses the server using the port
-func OpenPort(t *testing.T) {
-	// FIMXE: Implement this test
+	out, err := helpers.GetOutput("safescale network list")
+	fmt.Println(out)
+	require.Nil(t, err)
+
+	fmt.Println("Creating network " + names.Networks[0])
+
+	out, err = helpers.GetOutput("safescale network create " + names.Networks[0] + " --cidr 192.168.40.0/24")
+	fmt.Println(out)
+	require.Nil(t, err)
+
+	tn := fmt.Sprintf("gw-%s", names.Networks[0])
+	out, err = helpers.GetOutput(fmt.Sprintf("safescale ssh run -c \"wget https://github.com/svenstaro/miniserve/releases/download/v0.20.0/miniserve-v0.20.0-x86_64-unknown-linux-musl -O miniserve\" %s", tn))
+	fmt.Println(out)
+	require.Contains(t, out, "miniserve")
+	require.Contains(t, out, "saved")
+	require.Nil(t, err)
+
+	out, err = helpers.GetOutput(fmt.Sprintf("safescale ssh run -c \"chmod u+x ./miniserve\" %s", tn))
+	fmt.Println(out)
+	require.Nil(t, err)
+
+	out, err = helpers.GetOutput(fmt.Sprintf("safescale ssh run -c \"mkdir -p test\" %s", tn))
+	fmt.Println(out)
+	require.Nil(t, err)
+
+	out, err = helpers.GetOutput(fmt.Sprintf("safescale ssh run -c \"sudo /usr/bin/killall miniserve\" %s", tn))
+	if !strings.Contains(out, "no process found") {
+		require.Nil(t, err)
+	}
+
+	out, err = helpers.GetOutput(fmt.Sprintf("safescale ssh run -c \"nohup ./miniserve -p 7777 ./test > /dev/null 2>&1 &\" %s", tn))
+	fmt.Println(out)
+	require.Nil(t, err)
+
+	out, err = helpers.GetOutput(fmt.Sprintf("safescale host inspect %s", tn))
+	require.Nil(t, err)
+	fmt.Println(out)
+
+	pubip, err := helpers.RunJq(out, ".result.public_ip")
+	require.Nil(t, err)
+
+	// the port is initially closed
+	_, err = http.Get(fmt.Sprintf("http://%s:7777", pubip))
+	require.NotNil(t, err)
+
+	out, err = helpers.GetOutput(fmt.Sprintf("safescale host security group list %s", tn))
+	require.Nil(t, err)
+	fmt.Println(out)
+
+	var good string
+	first, err := helpers.RunJq(out, ".result[0].name")
+	require.Nil(t, err)
+
+	if strings.Contains(first, "publicip") {
+		good = ".result[0].id"
+	} else {
+		good = ".result[1].id"
+	}
+
+	theID, err := helpers.RunJq(out, good)
+	require.Nil(t, err)
+
+	// after that it should be open
+	out, err = helpers.GetOutput(fmt.Sprintf("safescale network security group rule add --direction ingress --port-from 7777 --port-to 7777 --cidr \"0.0.0.0/0\" --cidr \"0.0.0.0/0\" %s %s", tn, theID))
+	fmt.Println(out)
+	require.Nil(t, err)
+
+	// if it's open won't fail
+	resp, err := http.Get(fmt.Sprintf("http://%s:7777", pubip))
+	require.Nil(t, err)
+
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+
+	// make sure it works reading the content
+	body, err := ioutil.ReadAll(resp.Body)
+	require.Nil(t, err)
+	require.Contains(t, string(body), "miniserve")
 }
 
 func init() {
