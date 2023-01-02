71ad6a1fdcd9c8c6a19bed57f580635b173e5603
oscarpicas <oscar.picas@gmail.com>
2023-01-02 11:22:24 +0100

    chores: copyright update (#570)

--- integrationtests/resources/buckets/buckets_test.go
+++ integrationtests/resources/buckets/buckets_test.go
@@ -2,7 +2,7 @@
 // +build buckettests,integration allintegration
 
 /*
- * Copyright 2018-2022, CS Systemes d'Information, http://csgroup.eu
+ * Copyright 2018-2023, CS Systemes d'Information, http://csgroup.eu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
