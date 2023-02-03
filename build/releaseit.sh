1ed720f6bbe4d14a27cfa40a925784cab9759298
oscar <oscar.picas@gmail.com>
2023-02-03 15:47:46 +0100

    chores: changed build scripts

--- build/releaseit.sh
+++ build/releaseit.sh
@@ -11,7 +11,7 @@ GOOSX=windows GOARCHX=amd64 ./create-docker2.sh -f
 docker system prune -a -f
 mv ./exported-windows-amd64/safescale ./exported-windows-amd64/safescale.exe
 mv ./exported-windows-amd64/safescaled ./exported-windows-amd64/safescaled.exe
-tar -czf safescale-$1-linux-amd64.tar.gz ./exported/sa*
-tar -czf safescale-$1-linux-arm.tar.gz ./exported-linux-arm/sa*
-tar -czf safescale-$1-darwin-arm64.tar.gz ./exported-darwin-arm64/sa*
-tar -czf safescale-$1-windows-amd64.tar.gz ./exported-windows-amd64/sa*
+tar -czf safescale-$1-linux-amd64.tar.gz -C ./exported/safescale ./exported/safescaled .
+tar -czf safescale-$1-linux-arm.tar.gz -C ./exported-linux-arm/safescale ./exported-linux-arm/safescaled .
+tar -czf safescale-$1-darwin-arm64.tar.gz -C ./exported-darwin-arm64/safescale ./exported-darwin-arm64/safescaled .
+tar -czf safescale-$1-windows-amd64.tar.gz -C ./exported-windows-amd64/sa*.exe .
