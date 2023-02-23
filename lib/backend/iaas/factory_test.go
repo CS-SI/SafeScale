13d2088046c265e7df5480bd5aefe147d466a052
Matthieu Robert <matthieu.robert@csgroup.eu>
2023-02-23 15:02:38 +0100

    test : Create UT for factory.validateTenant() with basic valid tenant from different clients

--- lib/backend/iaas/factory_test.go
+++ lib/backend/iaas/factory_test.go
@@ -34,3 +34,121 @@ func Test_getTenantsFromCfg(t *testing.T) {
 
 	require.EqualValues(t, 30*time.Second, s.BigDelay())
 }
+
+func Test_validateAws(t *testing.T) {
+	v := viper.New()
+	v.AddConfigPath("./tenant_tests")
+	v.SetConfigName("aws")
+
+	tenants, _, _ := getTenantsFromViperCfg(v)
+
+	err := validateTenant(tenants[0])
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func Test_validateCloudferro(t *testing.T) {
+	v := viper.New()
+	v.AddConfigPath("./tenant_tests")
+	v.SetConfigName("cloudferro")
+
+	tenants, _, _ := getTenantsFromViperCfg(v)
+
+	err := validateTenant(tenants[0])
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func Test_validateFlexibleengine(t *testing.T) {
+	v := viper.New()
+	v.AddConfigPath("./tenant_tests")
+	v.SetConfigName("flexibleengine")
+
+	tenants, _, xerr := getTenantsFromViperCfg(v)
+
+	if xerr != nil {
+		t.Error(xerr.Error())
+	}
+
+	err := validateTenant(tenants[0])
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func Test_validateGcp(t *testing.T) {
+	v := viper.New()
+	v.AddConfigPath("./tenant_tests")
+	v.SetConfigName("gcp")
+
+	tenants, _, xerr := getTenantsFromViperCfg(v)
+
+	if xerr != nil {
+		t.Error(xerr.Error())
+	}
+
+	err := validateTenant(tenants[0])
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func Test_validateOpenstack(t *testing.T) {
+	v := viper.New()
+	v.AddConfigPath("./tenant_tests")
+	v.SetConfigName("openstack")
+
+	tenants, _, xerr := getTenantsFromViperCfg(v)
+
+	if xerr != nil {
+		t.Error(xerr.Error())
+	}
+
+	err := validateTenant(tenants[0])
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func Test_validateOutscale(t *testing.T) {
+	v := viper.New()
+	v.AddConfigPath("./tenant_tests")
+	v.SetConfigName("outscale")
+
+	tenants, _, xerr := getTenantsFromViperCfg(v)
+
+	if xerr != nil {
+		t.Error(xerr.Error())
+	}
+
+	err := validateTenant(tenants[0])
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func Test_validateOvh(t *testing.T) {
+	v := viper.New()
+	v.AddConfigPath("./tenant_tests")
+	v.SetConfigName("ovh")
+
+	tenants, _, xerr := getTenantsFromViperCfg(v)
+
+	if xerr != nil {
+		t.Error(xerr.Error())
+	}
+
+	err := validateTenant(tenants[0])
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
