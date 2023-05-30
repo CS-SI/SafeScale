c1dd42b0cf78f7996bcdcb2dae9f366a9af79060
oscar <oscar.picas@gmail.com>
2023-05-30 15:17:22 +0200

    clean: removed firewall rules used in tests

--- lib/backend/iaas/stacks/azuretf/templates/customcluster/firewall.tf
+++ lib/backend/iaas/stacks/azuretf/templates/customcluster/firewall.tf
@@ -13,19 +13,3 @@ resource "azurerm_network_security_rule" "SSH" {
 
   depends_on = [azurerm_resource_group.rg, azurerm_network_security_group.customcluster_network_sg]
 }
-
-resource "azurerm_network_security_rule" "flask" {
-  name                        = "${var.customcluster_name}-allow-flask"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "5000"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.customcluster_network_sg.name
-
-  depends_on = [azurerm_resource_group.rg, azurerm_network_security_group.customcluster_network_sg]
-}
